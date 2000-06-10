699a7cbf3fd46e04a33bafcc0366b0843373cb1a
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-06-10 16:32:37 +0000

    Remove functions that do no longer exist
    git-svn-id: svn://svn.cc65.org/cc65/trunk@51 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/cc65/codegen.h
+++ src/cc65/codegen.h
@@ -1,8 +1,35 @@
-/*
- * codegen.h
- *
- * Ullrich von Bassewitz, 04.06.1998
- */
+/*****************************************************************************/
+/*                                                                           */
+/*				   codegen.h				     */
+/*                                                                           */
+/*			      6502 code generator			     */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/* (C) 1998     Ullrich von Bassewitz                                        */
+/*              Wacholderweg 14                                              */
+/*              D-70597 Stuttgart                                            */
+/* EMail:       uz@musoftware.de                                             */
+/*                                                                           */
+/*                                                                           */
+/* This software is provided 'as-is', without any expressed or implied       */
+/* warranty.  In no event will the authors be held liable for any damages    */
+/* arising from the use of this software.                                    */
+/*                                                                           */
+/* Permission is granted to anyone to use this software for any purpose,     */
+/* including commercial applications, and to alter it and redistribute it    */
+/* freely, subject to the following restrictions:                            */
+/*                                                                           */
+/* 1. The origin of this software must not be misrepresented; you must not   */
+/*    claim that you wrote the original software. If you use this software   */
+/*    in a product, an acknowledgment in the product documentation would be  */
+/*    appreciated but is not required.                                       */
+/* 2. Altered source versions must be plainly marked as such, and must not   */
+/*    be misrepresented as being the original software.                      */
+/* 3. This notice may not be removed or altered from any source              */
+/*    distribution.                                                          */
+/*                                                                           */
+/*****************************************************************************/
 
 
 
@@ -366,15 +393,18 @@ void g_lt (unsigned flags, unsigned long val);
 void g_le (unsigned flags, unsigned long val);
 void g_gt (unsigned flags, unsigned long val);
 void g_ge (unsigned flags, unsigned long val);
-void g_makebool (unsigned flags);
-void outdat (int n);
+
 void g_res (unsigned n);
+/* Reserve static storage, n bytes */
 
 void g_defdata (unsigned flags, unsigned long val, unsigned offs);
 /* Define data with the size given in flags */
 
 void g_defbytes (const unsigned char *bytes, unsigned count);
+/* Output a row of bytes as a constant */
+
 void g_zerobytes (unsigned n);
+/* Output n bytes of data initialized with zero */
 
 
 

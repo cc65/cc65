c8193d636463b9ae0cb6ce23225cadad7cbc6514
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-06-10 16:13:50 +0000

    Remove/rename more code
    git-svn-id: svn://svn.cc65.org/cc65/trunk@49 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/runtime/leasp.s
+++ libsrc/runtime/leasp.s
@@ -4,22 +4,19 @@
 ; CC65 runtime: Load effective address with offset in Y relative to SP
 ;
 
-    	.export		leaasp, plea0sp, pleaysp
-    	.import		pushax
-    	.importzp	sp
+    	.export		leaasp, pleaysp
+     	.import		pushax
+     	.importzp	sp
 
 leaasp:	ldx	sp+1		; Get high byte
- 	clc
-	adc	sp
-   	bcc	@L1
-   	inx
-@L1:	rts
+     	clc
+     	adc	sp
+     	bcc	@L1
+     	inx
+@L1: 	rts
 
 
-plea0sp:
-	ldy	#0
-pleaysp:
-  	tya
+pleaasp:
   	ldx	sp+1		; Get high byte
   	clc
   	adc	sp

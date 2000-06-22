5453c45d8fdd983a836ffc54e3cdb994ac20b244
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-06-22 12:42:37 +0000

    Add the atari target
    git-svn-id: svn://svn.cc65.org/cc65/trunk@104 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/joystick.h
+++ include/joystick.h
@@ -15,6 +15,9 @@
 
 
 /* Define __JOYSTICK__ for systems that support a joystick */
+#ifdef __ATARI__
+#  define __JOYSTICK__
+#endif
 #ifdef __C64__
 #  define __JOYSTICK__
 #endif
@@ -22,9 +25,6 @@
 #  define __JOYSTICK__
 #endif
 #ifdef __PLUS4__
-#  define __JOYSTICK__
-#endif
-#ifdef __NES__
 #  define __JOYSTICK__
 #endif
 
@@ -35,22 +35,11 @@
 /* Result codes of the function. The actual code is a bitwise or
  * of one or more of the following values.
  */
-#ifdef __NES__
-#  define JOY_A		0x01
-#  define JOY_B		0x02
-#  define JOY_SELECT	0x04
-#  define JOY_START	0x08
-#  define JOY_UP	0x10
-#  define JOY_DOWN	0x20
-#  define JOY_LEFT	0x40
-#  define JOY_RIGHT	0x80
-#else
-#  define JOY_UP       	0x01
-#  define JOY_DOWN	0x02
-#  define JOY_LEFT	0x04
-#  define JOY_RIGHT	0x08
-#  define JOY_FIRE	0x10
-#endif
+#define JOY_UP       	0x01
+#define JOY_DOWN	0x02
+#define JOY_LEFT	0x04
+#define JOY_RIGHT	0x08
+#define JOY_FIRE	0x10
 
 
 

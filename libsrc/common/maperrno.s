9468b052314880fc04e21141880f358872f31699
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-06-24 09:10:16 +0000

    Separated _maperrno and _errno
    git-svn-id: svn://svn.cc65.org/cc65/trunk@121 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/common/maperrno.s
@@ -0,0 +1,27 @@
+;
+; Ullrich von Bassewitz, 24.06.2000
+;
+; void _maperrno(void);
+; /* Map an OS error to a system independent error code */
+;
+
+	.export		__maperrno
+	.import		__errno
+	.import		__oserror
+	.import		__osmaperrno
+
+
+.code
+
+__maperrno:
+	lda	__oserror		; Get the error code
+	beq	@L1			; Jump if no error
+	ldx	#$00			; Clear error
+	stx	__oserror
+	jsr	__osmaperrno		; Map the code
+       	sta    	__errno
+	stx	__errno+1
+@L1:	rts
+
+
+

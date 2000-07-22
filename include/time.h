6524692f3dc10c806ca5272844d69cf60f7ad0e2
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-07-22 19:01:53 +0000

    Added standard copyright header, fixed a wrong macro name.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@191 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/time.h
+++ include/time.h
@@ -1,9 +1,35 @@
-/*
- * time.h
- *
- * Ullrich von Bassewitz, 17.06.1998
- *
- */
+/*****************************************************************************/
+/*                                                                           */
+/*				    time.h				     */
+/*                                                                           */
+/*				 Date and time				     */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/* (C) 1998-2000 Ullrich von Bassewitz                                       */
+/*               Wacholderweg 14                                             */
+/*               D-70597 Stuttgart                                           */
+/* EMail:        uz@musoftware.de                                            */
+/*                                                                           */
+/*                                                                           */
+/* This software is provided 'as-is', without any expressed or implied       */
+/* warranty.  In no event will the authors be held liable for any damages    */
+/* arising from the use of this software.                                    */
+/*                                                                           */
+/* Permission is granted to anyone to use this software for any purpose,     */
+/* including commercial applications, and to alter it and redistribute it    */
+/* freely, subject to the following restrictions:                            */
+/*                                                                           */
+/* 1. The origin of this software must not be misrepresented; you must not   */
+/*    claim that you wrote the original software. If you use this software   */
+/*    in a product, an acknowledgment in the product documentation would be  */
+/*    appreciated but is not required.                                       */
+/* 2. Altered source versions must be plainly marked as such, and must not   */
+/*    be misrepresented as being the original software.                      */
+/* 3. This notice may not be removed or altered from any source              */
+/*    distribution.                                                          */
+/*                                                                           */
+/*****************************************************************************/
 
 
 
@@ -12,9 +38,11 @@
 
 
 
-#include <stddef.h>
-
-
+/* NULL pointer */
+#ifdef NULL
+#  undef NULL
+#endif
+#define NULL	0
 
 typedef unsigned long time_t;
 typedef unsigned long clock_t;
@@ -25,7 +53,7 @@ struct tm {
     int tm_min;
     int tm_hour;
     int tm_mday;
-    int tm_mon;
+    int tm_mon;		  
     int tm_year;
     int tm_wday;
     int tm_yday;
@@ -36,10 +64,10 @@ struct tm {
 #ifdef __CBM__
 #  ifdef __CBM610__
 #    define CLK_TCK    	       	50 	/* POSIX */
-#    define CLOCKS_PER_TICK	50	/* ANSI */
+#    define CLOCKS_PER_SEC	50	/* ANSI */
 #  else
-#    define CLK_TCK		60	/* POSIX */
-#    define CLOCKS_PER_TICK	60     	/* ANSI */
+#    define CLK_TCK	  	60	/* POSIX */
+#    define CLOCKS_PER_SEC	60     	/* ANSI */
 #  endif
 #endif
 

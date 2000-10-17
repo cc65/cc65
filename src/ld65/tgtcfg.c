84531f991fe9949ac2d65ed2fccc26c5bbc09193
cpg <cpg@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-10-17 23:51:01 +0000

    changed Atari ZP define to have max. ZP space available
    git-svn-id: svn://svn.cc65.org/cc65/trunk@376 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/ld65/tgtcfg.c
+++ src/ld65/tgtcfg.c
@@ -57,9 +57,9 @@ static const char CfgNone [] =
 
 static const char CfgAtari [] =
     "MEMORY {"
-       	"ZP: start = $D2, size = $1C, type = rw;"
+       	"ZP: start = $82, size = $7E, type = rw;"
         "HEADER: start = $0000, size = $6, file = %O;"
-        "RAM: start = $1F00, size = $9D1F, file = %O;"   /* 9D1F: matches upper bound BC1F */
+        "RAM: start = $1F00, size = $9D1F, file = %O;"  /* 9D1F: matches upper bound BC1F */
     "}"
     "SEGMENTS {"
         "EXEHDR: load = HEADER, type = wprot;"

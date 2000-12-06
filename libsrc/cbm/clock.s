023d6494908eb2591c2e2eb3f6925beb58f7e1fe
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2000-12-06 20:08:19 +0000

    Remove global namespace operator
    git-svn-id: svn://svn.cc65.org/cc65/trunk@562 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/cbm/clock.s
+++ libsrc/cbm/clock.s
@@ -13,9 +13,9 @@
 .proc	_clock
 
 	lda	#0  	    	; Byte 3 is always zero
-       	sta    	::sreg+1
+       	sta    	sreg+1
 	jsr	RDTIM
-	sty	::sreg
+	sty	sreg
 	rts	  		; Don't set CC, this has no meaning here
 
 .endproc

c6c752ef8bf822e29c74faf783f62e7f67359a96
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2001-03-26 20:47:12 +0000

    Added mor specialized decax functions
    git-svn-id: svn://svn.cc65.org/cc65/trunk@675 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/runtime/decax5.s
@@ -0,0 +1,18 @@
+;
+; Ullrich von Bassewitz, 26.03.2001
+;
+; CC65 runtime: Decrement ax by 5
+;
+
+       	.export	       	decax5
+
+	.macpack	generic
+
+.proc	decax5
+
+    	sub   	#5
+    	bcs   	@L9
+    	dex
+@L9:	rts
+
+.endproc
---
+++ libsrc/runtime/decax6.s
@@ -0,0 +1,18 @@
+;
+; Ullrich von Bassewitz, 26.03.2001
+;
+; CC65 runtime: Decrement ax by 6
+;
+
+       	.export	       	decax6
+
+	.macpack	generic
+
+.proc	decax6
+
+    	sub   	#6
+    	bcs   	@L9
+    	dex
+@L9:	rts
+
+.endproc

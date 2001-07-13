baf977dfd8c72079a8dbf10decc9c280d1f194ef
izydorst <izydorst@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2001-07-13 15:58:12 +0000

    multidrive support
    git-svn-id: svn://svn.cc65.org/cc65/trunk@778 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/geos/disk/dio_params.s
+++ libsrc/geos/disk/dio_params.s
@@ -3,11 +3,14 @@
 ; 2.7.2001
 ;
 ; this function is used by dio_read and dio_write to fix parameters (secnum)
-;
+; this function calls SetDevice so that more than one drive can be used at once
 
 	.import		popax,pushax,_dio_log_to_phys
+	.importzp	ptr1
  	.export		dio_params,dio_secnum
-	.include "../inc/geossym.inc"
+	.include 	"../inc/geossym.inc"
+	.include	"../inc/jumptab.inc"
+	.include	"../inc/dio.inc"
 
 .bss
 dio_secnum:	.res 2
@@ -21,6 +24,17 @@ dio_secnum:	.res 2
 	jsr popax
 	sta dio_secnum
 	stx dio_secnum
+
+	jsr popax		; get 3rd parameter
+	jsr pushax		; put it back
+	sta ptr1
+	stx ptr1+1
+	ldy #sst_driveno
+	lda (ptr1),y
+	clc
+	adc #8
+	jsr SetDevice		; setup device, load driver
+
 	lda #<dio_secnum
 	ldx #>dio_secnum
 	jsr pushax

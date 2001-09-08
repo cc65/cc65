522ddabc35139565eea327b6514f199c6b3e7363
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2001-09-08 20:51:33 +0000

    Add new defines for export usage
    git-svn-id: svn://svn.cc65.org/cc65/trunk@875 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/common/symdefs.h
+++ src/common/symdefs.h
@@ -49,35 +49,43 @@
 
 
 /* Import size */
-#define IMP_ABS       	0x00   	       	/* Import as normal value */
-#define IMP_ZP 	      	0x01		/* Import as zero page symbol */
-#define IMP_MASK_SIZE 	0x01		/* Size mask */
+#define IMP_ABS       	        0x00   	/* Import as normal value */
+#define IMP_ZP 	      	        0x01   	/* Import as zero page symbol */
+#define IMP_MASK_SIZE 	        0x01   	/* Size mask */
 
-#define IS_IMP_ABS(x) 	(((x) & IMP_MASK_SIZE) == IMP_ABS)
-#define IS_IMP_ZP(x)  	(((x) & IMP_MASK_SIZE) == IMP_ZP)
-
-/* Export size */
-#define EXP_ABS	       	0x00		/* Export as normal value */
-#define EXP_ZP 	       	0x08   	       	/* Export as zero page value */
-#define EXP_MASK_SIZE 	0x08		/* Size mask */
-
-#define IS_EXP_ABS(x)  	(((x) & EXP_MASK_SIZE) == EXP_ABS)
-#define IS_EXP_ZP(x)  	(((x) & EXP_MASK_SIZE) == EXP_ZP)
-
-/* Export value type */
-#define EXP_CONST     	0x00		/* Mask bit for const values */
-#define EXP_EXPR      	0x10   	 	/* Mask bit for expr values */
-#define EXP_MASK_VAL  	0x10		/* Value mask */
-
-#define IS_EXP_CONST(x)	(((x) & EXP_MASK_VAL) == EXP_CONST)
-#define IS_EXP_EXPR(x) 	(((x) & EXP_MASK_VAL) == EXP_EXPR)
+#define IS_IMP_ABS(x)  	        (((x) & IMP_MASK_SIZE) == IMP_ABS)
+#define IS_IMP_ZP(x)  	        (((x) & IMP_MASK_SIZE) == IMP_ZP)
 
 /* Number of module constructor/destructor declarations for an export */
-#define EXP_CONDES_MASK	0x07
+#define EXP_CONDES_MASK	        0x07
 
 #define IS_EXP_CONDES(x)	(((x) & EXP_CONDES_MASK) != 0)
 #define GET_EXP_CONDES_COUNT(x)	((x) & EXP_CONDES_MASK)
 #define INC_EXP_CONDES_COUNT(x) ((x)++)
+
+/* Export size */
+#define EXP_ABS	       	        0x00   	/* Export as normal value */
+#define EXP_ZP 	       	        0x08   	/* Export as zero page value */
+#define EXP_MASK_SIZE 	        0x08   	/* Size mask */
+
+#define IS_EXP_ABS(x)  	        (((x) & EXP_MASK_SIZE) == EXP_ABS)
+#define IS_EXP_ZP(x)   	        (((x) & EXP_MASK_SIZE) == EXP_ZP)
+
+/* Export value type */
+#define EXP_CONST     	        0x00   	/* Mask bit for const values */
+#define EXP_EXPR      	        0x10   	/* Mask bit for expr values */
+#define EXP_MASK_VAL  	        0x10   	/* Value mask */
+
+#define IS_EXP_CONST(x)	        (((x) & EXP_MASK_VAL) == EXP_CONST)
+#define IS_EXP_EXPR(x) 	        (((x) & EXP_MASK_VAL) == EXP_EXPR)
+
+/* Export usage */
+#define EXP_EQUATE              0x00   	/* Mask bit for an equate */
+#define EXP_LABEL               0x20    /* Mask bit for a label */
+#define EXP_MASK_LABEL          0x20    /* Value mask */
+
+#define IS_EXP_EQUATE(x)        (((x) & EXP_MASK_LABEL) == EXP_EQUATE)
+#define IS_EXP_LABEL(x)         (((x) & EXP_MASK_LABEL) == EXP_LABEL)
 
 
 

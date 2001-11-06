56d98bbb934bfdd6a5981f301118e1c22b27aae9
cpg <cpg@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2001-11-06 01:22:54 +0000

    return the hardware tv system, not the ROMs one
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1099 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/atari/tvtype.s
+++ libsrc/atari/tvtype.s
@@ -1,9 +1,11 @@
 ;
-; Christian Groessler, December 2000
+; Christian Groessler, November 2001
 ;
 ; unsigned char get_tv(void)
 ;
 ; returns the TV system the machine is using
+; 0 - NTSC
+; 1 - PAL
 ;
 
 
@@ -12,8 +14,13 @@
 
 .proc	_get_tv
 
-	lda	PALNTS
+	lda	PAL		; use hw register, PALNTS is only supported on XL/XE ROM
 	ldx	#0
+	and	#$0e
+	bne	@NTSC
+	lda	#1
+	rts
+@NTSC:	txa
 	rts
 
 .endproc

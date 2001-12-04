a86b97cc89c30deb53c302bda1240a10a60b1f73
cpg <cpg@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2001-12-04 01:04:20 +0000

    fixed return paramter; setup ICAX1Z so the key routine isn't confused by opened R: devices
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1129 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/atari/cgetc.s
+++ libsrc/atari/cgetc.s
@@ -8,10 +8,14 @@
         .export _cgetc
         
 _cgetc:
-        lda     KEYBDV+5
+	jsr	@1
+	ldx	#0
+	rts
+
+@1:	lda     KEYBDV+5
         pha
         lda     KEYBDV+4
         pha
+	lda	#12
+	sta	ICAX1Z		; fix problems with direct call to KEYBDV
         rts
-	ldx	#0
-	rts

9556117f0f95c1186cd4561af9dcb3b3b8c82d93
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-05-26 08:32:47 +0000

    zpspace cannot be an external symbol
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1289 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/runtime/zeropage.s
+++ libsrc/runtime/zeropage.s
@@ -7,16 +7,16 @@
 	.exportzp   	sp, sreg, regsave
 	.exportzp   	ptr1, ptr2, ptr3, ptr4
 	.exportzp   	tmp1, tmp2, tmp3, tmp4
-	.exportzp   	regbank, zpspace
+	.exportzp   	regbank
 
 ; ------------------------------------------------------------------------
 
 .zeropage
 
 zpstart	= *
-sp:	      	.res   	2 	; Stack pointer
-sreg:	      	.res	2	; Secondary register/high 16 bit for longs
-regsave:      	.res	2	; slot to save/restore (E)AX into
+sp:	      	.res   	2      	; Stack pointer
+sreg:	      	.res	2      	; Secondary register/high 16 bit for longs
+regsave:      	.res	2      	; slot to save/restore (E)AX into
 ptr1:	      	.res	2
 ptr2:	      	.res	2
 ptr3:	      	.res	2
@@ -25,7 +25,5 @@ tmp1:	      	.res	1
 tmp2:	      	.res	1
 tmp3:	      	.res	1
 tmp4:	      	.res	1
-regbank:      	.res	6	; 6 byte register bank
-
-zpspace	= * - zpstart		; Zero page space allocated
+regbank:      	.res	6      	; 6 byte register bank
 

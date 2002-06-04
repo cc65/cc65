8d8ec57003feb447c72f0e4c8fa337776ad252ca
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-06-04 07:03:38 +0000

    Added atmos.h from Jede
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1299 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ include/atmos.h
@@ -0,0 +1,61 @@
+/*****************************************************************************/
+/*                                                                           */
+/*				   atmos.h                                   */
+/*                                                                           */
+/*		     Oric Atmos system specific definitions                  */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/* (C) 2002  Debrune Jérome, <jede@oric.org>		                     */
+/*                                                                           */
+/*                                                                           */
+/* This software is provided 'as-is', without any expressed or implied       */
+/* warranty.  In no event will the authors be held liable for any damages    */
+/* arising from the use of this software.                                    */
+/*                                                                           */
+/* Permission is granted to anyone to use this software for any purpose,     */
+/* including commercial applications, and to alter it and redistribute it    */
+/* freely, subject to the following restrictions:                            */
+/*                                                                           */
+/* 1. The origin of this software must not be misrepresented; you must not   */
+/*    claim that you wrote the original software. If you use this software   */
+/*    in a product, an acknowledgment in the product documentation would be  */
+/*    appreciated but is not required.                                       */
+/* 2. Altered source versions must be plainly marked as such, and must not   */
+/*    be misrepresented as being the original software.                      */
+/* 3. This notice may not be removed or altered from any source              */
+/*    distribution.                                                          */
+/*                                                                           */
+/*****************************************************************************/
+
+
+
+#ifndef _ATMOS_H
+#define _ATMOS_H
+
+
+
+/* Check for errors */
+#if !defined(__ATMOS__)
+#  error This module may only be used when compiling for the Oric Atmos !
+#endif
+
+
+
+/* Color defines */
+#define COLOR_BLACK 	0x00
+#define COLOR_RED 	0x01
+#define COLOR_GREEN 	0x02
+#define COLOR_YELLOW 	0x03
+#define COLOR_BLUE 	0x04
+#define COLOR_MAGENTA 	0x05
+#define COLOR_CYAN 	0x06
+#define COLOR_WHITE 	0x07
+
+
+
+/* End of atmos.h */
+#endif
+
+
+

62b3576f5f015a7791d6997d4d67ac98369b4008
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-10-06 18:59:08 +0000

    Use other representations for SCHAR_MIN, SHORT_MIN, INT_MIN and LONG_MIN. Among other things that avoids the compiler warning "constant is long" when using INT_MIN.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1447 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/limits.h
+++ include/limits.h
@@ -6,7 +6,7 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 1998-2000 Ullrich von Bassewitz                                       */
+/* (C) 1998-2002 Ullrich von Bassewitz                                       */
 /*               Wacholderweg 14                                             */
 /*               D-70597 Stuttgart                                           */
 /* EMail:        uz@musoftware.de                                            */
@@ -40,26 +40,26 @@
 
 #define CHAR_BIT	8
 
-#define SCHAR_MIN	(-128)
-#define SCHAR_MAX	127
+#define SCHAR_MIN	((signed char) 0x80)
+#define SCHAR_MAX	127                
 
 #define UCHAR_MAX	255
 
 #define CHAR_MIN	0
 #define CHAR_MAX	255
 
-#define SHRT_MIN	(-32768)
+#define SHRT_MIN       	((short) 0x8000)
 #define SHRT_MAX	32767
 
 #define USHRT_MAX	65535U
 
-#define INT_MIN		(-32768)
+#define INT_MIN	       	((int) 0x8000)
 #define INT_MAX		32767
 
 #define UINT_MAX	65535U
 
 #define LONG_MAX	2147483647L
-#define LONG_MIN	(-2147483648L)
+#define LONG_MIN       	((long) 0x8000000)
 
 #define ULONG_MAX	4294967295UL
 

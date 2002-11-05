511b3b45f2fa0b72a3ae6b8a5a663b145fc5eb1b
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-11-05 20:23:06 +0000

    Add _randomize. Make realloc __fastcall__. Make identifiers with a leading underscore visible even if __STRICT_ANSI__ is defined, since those don't pollute the application namespace.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1491 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/stdlib.h
+++ include/stdlib.h
@@ -55,16 +55,15 @@ typedef struct {
 /* Memory management */
 void* __fastcall__ malloc (size_t size);
 void* __fastcall__ calloc (size_t count, size_t size);
-void* realloc (void* block, size_t size);
+void* __fastcall__ realloc (void* block, size_t size);
 void __fastcall__ free (void* block);
-#ifndef __STRICT_ANSI__
 void __fastcall__ _hadd (void* mem, size_t size);	/* Non-standard */
-#endif
 
 /* Random numbers */
-#define	RAND_MAX	0x7FFF
+#define	RAND_MAX   	0x7FFF
 int rand (void);
 void __fastcall__ srand (unsigned seed);
+void _randomize (void);         /* Non-standard */
 
 /* Other standard stuff */
 void abort (void);
@@ -82,8 +81,8 @@ void qsort (void* base, size_t count, size_t size,
 	    int (*compare) (const void*, const void*));
 
 /* Non-ANSI functions */
-#ifndef __STRICT_ANSI__
 void __fastcall__ _swap (void* p, void* q, size_t size);
+#ifndef __STRICT_ANSI__
 char* __fastcall__ itoa (int val, char* buf, int radix);
 char* __fastcall__ utoa (unsigned val, char* buf, int radix);
 char* __fastcall__ ltoa (long val, char* buf, int radix);

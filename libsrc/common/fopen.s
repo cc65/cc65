6195f87f6a1344e9c00c5c58c04c56588cd07242
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-11-23 18:46:40 +0000

    Rewrote fopen in assembler
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1608 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/common/fopen.s
@@ -0,0 +1,51 @@
+;
+; Ullrich von Bassewitz, 22.11.2002
+;
+; FILE* __fastcall__ fopen (const char* name, const char* mode)
+; /* Open a file */
+;
+
+        .export         _fopen
+
+        .import         __fopen, __fdesc
+        .import         pushax, return0
+
+        .include        "errno.inc"
+
+
+; ------------------------------------------------------------------------
+; Code
+
+.proc   _fopen
+
+; Bring the mode parameter on the stack
+
+        jsr     pushax
+
+; Allocate a new file stream
+
+        jsr     __fdesc
+
+; Check if we have a stream
+
+        cmp     #$00
+        bne     @L1
+        cpx     #$00
+        bne     @L1
+
+; Failed to allocate a file stream
+
+        lda     #EMFILE
+        sta     __errno
+        lda     #0
+        sta     __errno+1
+        tax
+        rts                     ; Return zero
+
+; Open the file and return the file descriptor. All arguments are already
+; in place: name and mode on the stack, and f in a/x
+
+@L1:    jmp     __fopen
+
+.endproc
+

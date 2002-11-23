222d3d1f23c32596414e9aac7a8c46852e3ff113
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2002-11-23 16:21:46 +0000

    Rewrite
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1605 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/runtime/pushw.s
+++ libsrc/runtime/pushw.s
@@ -1,22 +1,18 @@
 ;
 ; Ullrich von Bassewitz, 31.08.1998
 ;
-; CC65 runtime: Push word from stack
+; CC65 runtime: Fetch word indirect and push
 ;
 
 	.export		pushw, pushwidx
 	.import		pushax
 	.importzp	ptr1
 
+
+pushw:  ldy     #1
 pushwidx:
-	sty	ptr1
-	clc
-	adc	ptr1
-	bcc	pushw
-	inx
-pushw:	sta	ptr1
+        sta	ptr1
 	stx	ptr1+1
-	ldy	#1
 	lda	(ptr1),y
 	tax
 	dey

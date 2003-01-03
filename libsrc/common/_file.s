91e960f3918f093e5c650827705f42e687ee577c
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-01-03 07:58:25 +0000

    Removed closeallstreams
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1874 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/_file.s
+++ libsrc/common/_file.s
@@ -3,45 +3,11 @@
 ;
 ; Data for the stdio file stream.
 ;
-; Be sure to keep the value priority of closeallstreams lower than that of
-; closeallfiles (which is the low level POSIX counterpart and must be
-; called after closeallstreams).
 
 	.export	 	__filetab, _stdin, _stdout, _stderr
-        .destructor     closeallstreams, 16
-        .import         _close
 
         .include        "fcntl.inc"
         .include        "_file.inc"
-
-;----------------------------------------------------------------------------
-; Close all files on exit
-
-.proc   closeallstreams
-
-       	ldy	#((FOPEN_MAX - 1) * _FILE_size)
-loop:  	sty     index                           ; Save the index
-        lda     __filetab + _FILE_f_flags,y     ; Load file flags
-        and     #_FOPEN                         ; Is it open?
-       	beq    	next                            ; jump if closed
-
-; Close this file
-
-        lda     __filetab + _FILE_f_fd,y
-        ldx     #0
-        jsr     _close
-
-; Next file
-
-next:   lda     index
-        sec
-        sbc     #_FILE_size
-        tay
-        bcs     loop
-
-        rts
-
-.endproc
 
 ;----------------------------------------------------------------------------
 ; File data
@@ -68,9 +34,4 @@ _stdout:
 _stderr:
 	.word	__filetab + (STDERR_FILENO * _FILE_size)
 
-
-; Temp storage for closeallstreams
-
-.bss
-index:  .res    1
 

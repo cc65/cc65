2a567c4a3225faa65a7012e000d7140555da5cc4
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-02-16 14:31:03 +0000

    Removed an unused constant. Added an empty EXTZP segment.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@1986 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/runtime/zeropage.s
+++ libsrc/runtime/zeropage.s
@@ -13,7 +13,6 @@
 
 .zeropage
 
-zpstart	= *
 sp:	      	.res   	2      	; Stack pointer
 sreg:	      	.res	2      	; Secondary register/high 16 bit for longs
 regsave:      	.res	4      	; slot to save/restore (E)AX into
@@ -26,4 +25,10 @@ tmp2:	      	.res	1
 tmp3:	      	.res	1
 tmp4:	      	.res	1
 regbank:      	.res	6      	; 6 byte register bank
+
+; Add an empty EXTZP zeropage segment to avoid linker warnings that this
+; segment does not exist (it does not exist in most builtin linker configs
+; but is used when linking modules).
+
+.segment        "EXTZP", zeropage
 

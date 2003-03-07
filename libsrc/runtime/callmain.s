9257570050fd4f402a9f3152c86d12e46325e848
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-03-07 11:53:17 +0000

    New module
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2010 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/runtime/callmain.s
@@ -0,0 +1,40 @@
+;
+; Ullrich von Bassewitz, 2003-03-07
+;
+; Push arguments and call main()
+;
+
+
+	.export         callmain
+       	.export		__argc, __argv
+
+        .import         _main, pushax
+
+;---------------------------------------------------------------------------
+; Setup the stack for main(), then jump to it
+
+.proc   callmain
+
+        lda     __argc
+        ldx     __argc+1
+        jsr     pushax          ; Push argc
+
+        lda     __argv
+        ldx     __argv+1
+        jsr     pushax          ; Push argv
+
+	ldy	#4   	  	; Argument size
+        jmp     _main
+
+.endproc
+
+;---------------------------------------------------------------------------
+; Data
+
+.bss
+__argc:         .res    2
+__argv:         .res    2
+
+
+
+

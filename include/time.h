4fbcb437635ce5ee72da16dbe61b5dfb78579071
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-04-15 20:47:12 +0000

    NES changes
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2057 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/time.h
+++ include/time.h
@@ -93,6 +93,9 @@ unsigned _clocks_per_sec (void);
 #    define CLK_TCK	  	60	/* POSIX */
 #    define CLOCKS_PER_SEC	60     	/* ANSI */
 #  endif
+#elif defined(__NES__)
+#  define CLK_TCK    	       	50 	/* POSIX */
+#  define CLOCKS_PER_SEC	50	/* ANSI */
 #endif
 
 

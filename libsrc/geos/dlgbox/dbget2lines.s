aaf1db13dd7d87816492ef80a327d045b4871afd
izydorst <izydorst@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-05-01 16:39:09 +0000

    DlgBox* with 2lines arguments will accept both "" and NULL for a blank line
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2107 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/geos/dlgbox/dbget2lines.s
+++ libsrc/geos/dlgbox/dbget2lines.s
@@ -1,6 +1,6 @@
 
 ;
-; Maciej 'YTM/Alliance' Witkowiak
+; Maciej 'YTM/Elysium' Witkowiak
 ;
 ; 25.12.99
 
@@ -8,10 +8,30 @@
 	    .importzp ptr3,ptr4
 	    .import popax
 
+.code
+
 DB_get2lines:
-	    sta ptr4
+	    sta ptr4		; fetch line 2
 	    stx ptr4+1
 	    jsr popax
-	    sta ptr3
+	    sta ptr3		; fetch line 1
 	    stx ptr3+1
-	    rts
+	    ldx ptr3
+	    jsr checknull
+	    ldx ptr4
+	    jmp checknull
+
+checknull:
+	    lda $0,x
+	    ora $1,x
+	    bne cn_end
+	    lda #<nullstring
+	    sta $0,x
+	    lda #>nullstring
+	    sta $1,x
+cn_end:	    rts
+
+.rodata
+
+nullstring:
+	    .byte 0

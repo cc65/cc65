741362f830f40444eb7be745011b1acd2d22b26f
izydorst <izydorst@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-05-16 15:26:49 +0000

    forgotten name change; fixed to return more verbose information instead of boolean true/false
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2161 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/geos/mousesprite/ismseinregion.s
+++ libsrc/geos/mousesprite/ismseinregion.s
@@ -6,16 +6,14 @@
 
 ; char IsMseInRegion (struct window *mywindow);
 
-	    .import RectRegs
-	    .import return0, return1
+	    .import _InitDrawWindow
 
 	    .export _IsMseInRegion
 	    
 	    .include "../inc/jumptab.inc"
 
 _IsMseInRegion:
-	    jsr RectRegs
+	    jsr _InitDrawWindow
 	    jsr IsMseInRegion
-	    bne L1
-	    jmp return0
-L1:	    jmp return1
+	    ldx #0
+	    rts

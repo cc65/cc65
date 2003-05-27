975eefc7c2f2b07572042b27a63b5e5b43afca5b
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-05-27 18:23:38 +0000

    Added a dummy system() function
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2171 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/common/system.c
@@ -0,0 +1,20 @@
+/*
+ * system.c
+ *
+ * Stefan Haubenthal, 2003-05-26
+ * Ullrich von Bassewitz, 2003-05-27
+ */
+
+#include <stdlib.h>
+
+int system (const char* s)
+{
+    if (s == NULL) {
+        return 0;               /* no shell */
+    } else {
+        return -1;              /* always fail */
+    }
+}
+
+
+

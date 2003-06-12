a8534fbb7c23d22610745c34f49643b2a4ed5105
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-06-12 18:17:46 +0000

    sleep.c doesn't compile on platforms without a clock function
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2218 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/sleep.c
+++ libsrc/common/sleep.c
@@ -8,17 +8,20 @@
 
 
 
-#include <time.h>     
+#include <time.h>
 #include <unistd.h>
 
 
 
+/* We cannot implement this function without a working clock function */
+#if defined(CLOCKS_PER_SEC)
 unsigned __fastcall__ sleep (unsigned wait)
 {
     clock_t goal = clock () + ((clock_t) wait) * CLOCKS_PER_SEC;
     while ((long) (goal - clock ()) > 0) ;
     return 0;
 }
+#endif
 
 
 

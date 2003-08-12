deeecae2cea82eb01d402ce6d3299b4b772619af
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-08-12 17:42:03 +0000

    Added get_ostype.s
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2289 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/c64/get_ostype.s
@@ -0,0 +1,22 @@
+;
+; Stefan Haubenthal, Jul 10 2003
+;
+; unsigned char get_ostype(void)
+;
+; $AA US
+; $64 PET-64
+; $43 SX-64
+; $03 EU_NEW
+; $00 EU_OLD
+;
+
+	.export		_get_ostype
+
+.proc	_get_ostype
+
+	lda	$ff80
+	rts
+
+.endproc
+
+

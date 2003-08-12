651e82ac7816b1014dade2265bf72f4c8f7af596
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-08-12 17:40:37 +0000

    New get_ostype function
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2288 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/c64.h
+++ include/c64.h
@@ -6,10 +6,10 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 1998-2002 Ullrich von Bassewitz                                       */
-/*               Wacholderweg 14                                             */
-/*               D-70597 Stuttgart                                           */
-/* EMail:        uz@musoftware.de                                            */
+/* (C) 1998-2003 Ullrich von Bassewitz                                       */
+/*               Römerstrasse 52                                             */
+/*               D-70794 Filderstadt                                         */
+/* EMail:        uz@cc65.org                                                 */
 /*                                                                           */
 /*                                                                           */
 /* This software is provided 'as-is', without any expressed or implied       */
@@ -45,6 +45,12 @@
 
 
 
+/*****************************************************************************/
+/*	       	   	  	     Data				     */
+/*****************************************************************************/
+
+
+
 /* Additional key defines */
 #define CH_F1			133
 #define CH_F2			137
@@ -54,8 +60,6 @@
 #define CH_F6			139
 #define CH_F7			136
 #define CH_F8			140
-
-
 
 /* Color defines */
 #define COLOR_BLACK  	       	0x00
@@ -75,8 +79,6 @@
 #define COLOR_LIGHTBLUE      	0x0E
 #define COLOR_GRAY3  	       	0x0F
 
-
-
 /* Define hardware */
 #include <_vic2.h>
 #define VIC    	(*(struct __vic2*)0xD000)
@@ -88,15 +90,31 @@
 #define CIA1	(*(struct __6526*)0xDC00)
 #define CIA2	(*(struct __6526*)0xDD00)
 
-
-
 /* Define special memory areas */
 #define COLOR_RAM	((unsigned char*)0xD800)
+
+/* Return codes for get_ostype */
+#define C64_OS_US       0xAA            /* US version */
+#define C64_OS_PET64    0x64            /* PET 64 */
+#define C64_OS_SX64     0x43            /* SX-64 */
+#define C64_EU_NEW      0x03            
+#define C64_EU_OLD      0x00
+
+
+
+/*****************************************************************************/
+/*	       	   		     Code				     */
+/*****************************************************************************/
+
+
+
+unsigned char get_ostype (void);
+/* Get the ROM version. Returns one of the C64_OS_xxx codes. */
 
 
 
 /* End of c64.h */
 #endif
-                                
+
 
 

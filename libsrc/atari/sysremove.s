93f1a5b61aa336aa4eabbc4d3cdb5ba4b6ff1acc
cpg <cpg@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-08-17 19:56:14 +0000

    renamed remove.s to sysremove.s and adapted it for common/remove.s
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2354 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/atari/sysremove.s
@@ -0,0 +1,79 @@
+;
+; Christian Groessler, Aug-2003
+;
+; int remove (const char* name);
+;
+
+	.include "atari.inc"
+	.include "errno.inc"
+	.import	findfreeiocb
+	.importzp tmp2
+.ifdef	UCASE_FILENAME
+	.importzp tmp3
+	.import	addysp
+	.import	ucase_fn
+.endif
+	.export	__sysremove
+
+.proc	__sysremove
+
+	pha			; save input parameter
+	txa
+	pha
+
+	jsr	findfreeiocb
+	beq	iocbok		; we found one
+
+	pla
+	pla			; fix up stack
+
+	lda	#TMOF		; too many open files
+	rts
+
+iocbok:	stx	tmp2		; remember IOCB index
+	pla
+	tax
+	pla			; get argument again
+
+.ifdef	UCASE_FILENAME
+
+	jsr	ucase_fn
+	bcc	ucok1
+
+	lda	#177		; see oserror.s
+	rts
+ucok1:
+
+.endif	; defined UCASE_FILENAME
+
+	ldy	tmp2		; IOCB index
+	sta	ICBAL,y		; store pointer to filename
+	txa
+	sta	ICBAH,y
+	tya
+	tax
+	lda	#DELETE
+	sta	ICCOM,x
+	lda	#0
+	sta	ICAX1,x
+	sta	ICAX2,x
+	sta	ICBLL,x
+	sta	ICBLH,x
+	jsr	CIOV
+
+.ifdef	UCASE_FILENAME
+	tya
+	pha
+	ldy	tmp3		; get size
+	jsr	addysp		; free used space on the stack
+	pla
+	tay
+.endif	; defined UCASE_FILENAME
+
+	bmi	cioerr
+	lda	#0
+	rts
+cioerr:	tya
+	rts
+
+.endproc	; __sysremove

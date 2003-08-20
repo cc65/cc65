6c65ea7d4a82b00200bfba4ad9c50ede441cd3fc
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-08-20 12:04:31 +0000

    memcpy and memmove have been split
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2386 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/geos/common/memcpy.s
+++ libsrc/geos/common/memcpy.s
@@ -1,12 +1,10 @@
 ;
-; void* memcpy (void* dest, const void* src, size_t n);
-; void* memmove (void* dest, const void* src, size_t n);
+; void* __fastcall__ memcpy (void* dest, const void* src, size_t n);
 ;
 ; Maciej 'YTM/Elysium' Witkowiak, 15.07.2001
 ;
 
-  	.export		_memcpy, _memmove
+  	.export		_memcpy
 	.import		_MoveData
 
 _memcpy	= _MoveData
-_memmove = _MoveData
---
+++ libsrc/geos/common/memmove.s
@@ -0,0 +1,10 @@
+;
+; void* __fastcall__ memmove (void* dest, const void* src, size_t n);
+;
+; Maciej 'YTM/Elysium' Witkowiak, 15.07.2001
+;
+
+  	.export		_memmove
+	.import		_MoveData
+
+_memmove = _MoveData

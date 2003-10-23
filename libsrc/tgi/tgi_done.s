d5dc5a75523b7f5bf9c6104e35affdc060d2a8aa
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-10-23 09:38:51 +0000

    DONE does no longer set an error code
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2570 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/tgi/tgi_done.s
+++ libsrc/tgi/tgi_done.s
@@ -12,11 +12,8 @@
         lda     _tgi_gmode              ; Is a graphics mode active?
         beq     @L1                     ; Jump if not
         jsr     tgi_done                ; Call the driver routine
-        jsr     tgi_geterror            ; Get the error code
-        sta     _tgi_error              ; Save it for reference
-        cmp     #TGI_ERR_OK
-        bne     @L1                     ; Jump if we had an error
-        sta     _tgi_gmode              ; Reset the graph mode flag (A = 0)
+        lda     #$00
+        sta     _tgi_gmode              ; Reset the graph mode flag
 @L1:    rts
 
 .endproc

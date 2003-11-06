cd8d69d1b047aecf19203fbeec9a39ba9f4c6a3e
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2003-11-06 20:17:03 +0000

    New module
    git-svn-id: svn://svn.cc65.org/cc65/trunk@2617 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ src/common/addrsize.h
@@ -0,0 +1,59 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                addrsize.h                                 */
+/*                                                                           */
+/*                         Address size definitions                          */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/* (C) 1998-2003 Ullrich von Bassewitz                                       */
+/*               Römerstraße 52                                              */
+/*               D-70794 Filderstadt                                         */
+/* EMail:        uz@cc65.org                                                 */
+/*                                                                           */
+/*                                                                           */
+/* This software is provided 'as-is', without any expressed or implied       */
+/* warranty.  In no event will the authors be held liable for any damages    */
+/* arising from the use of this software.                                    */
+/*                                                                           */
+/* Permission is granted to anyone to use this software for any purpose,     */
+/* including commercial applications, and to alter it and redistribute it    */
+/* freely, subject to the following restrictions:                            */
+/*                                                                           */
+/* 1. The origin of this software must not be misrepresented; you must not   */
+/*    claim that you wrote the original software. If you use this software   */
+/*    in a product, an acknowledgment in the product documentation would be  */
+/*    appreciated but is not required.                                       */
+/* 2. Altered source versions must be plainly marked as such, and must not   */
+/*    be misrepresented as being the original software.                      */
+/* 3. This notice may not be removed or altered from any source              */
+/*    distribution.                                                          */
+/*                                                                           */
+/*****************************************************************************/
+
+
+
+#ifndef ADDRSIZE_H
+#define ADDRSIZE_H
+
+
+
+/*****************************************************************************/
+/*     	       	    		     Data				     */
+/*****************************************************************************/
+
+
+
+#define ADDR_SIZE_DEFAULT       0x00
+#define ADDR_SIZE_ZEROPAGE      0x01
+#define ADDR_SIZE_ABSOLUTE      0x02
+#define ADDR_SIZE_FAR           0x03
+
+
+
+/* End of addrsize.h */
+
+#endif
+
+
+

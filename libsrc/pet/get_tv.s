b5db6e9e7bff2add992020c9ecdb649126a6c781
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2004-10-26 19:07:38 +0000

    New function get_tv
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3260 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/pet/get_tv.s
@@ -0,0 +1,22 @@
+;
+; Ullrich von Bassewitz, 2004-10-26
+;
+; unsigned char __fastcall__ get_tv (void);
+; /* Return the video mode the machine is using */
+;
+
+        .include        "pet.inc"
+	.include	"get_tv.inc"
+
+;--------------------------------------------------------------------------
+; _get_tv
+
+.proc   _get_tv
+
+        lda     #TV::OTHER
+        ldx     #0
+        rts
+
+.endproc
+
+

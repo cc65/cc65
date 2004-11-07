f044f9d6b592eb7334dc6c95e8b571c5bbbb3860
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2004-11-07 10:45:24 +0000

    Added the irq vector
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3282 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/tgi/tgi-kernel.h
+++ include/tgi/tgi-kernel.h
@@ -6,7 +6,7 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 2002-2003 Ullrich von Bassewitz                                       */
+/* (C) 2002-2004 Ullrich von Bassewitz                                       */
 /*               Römerstraße 52                                              */
 /*               D-70794 Filderstadt                                         */
 /* EMail:        uz@cc65.org                                                 */
@@ -77,6 +77,7 @@ typedef struct {
     void*               line;           /* LINE routine */
     void*               bar;            /* BAR routine */
     void*               circle;         /* CIRCLE routine */
+    void*               irq;            /* IRQ routine */
 
 } tgi_drv_header;
 

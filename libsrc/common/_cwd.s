19e2d5601669d933d6178f44d704e0fddaad5fdb
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2005-04-16 14:43:22 +0000

    Initialize _cwd by use of a constructor that calls a platform specific function.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3461 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/_cwd.s
+++ libsrc/common/_cwd.s
@@ -1,15 +1,23 @@
 ;
-; Ullrich von Bassewitz, 2003-08-12
+; Ullrich von Bassewitz, 2003-08-12, 2005-04-16
 ;
 ; Place to store the current working directory.
-; NOTE: Some of the code working with directories is not able to handle 
+; NOTE: Some of the code working with directories is not able to handle
 ; strings longer than 255 chars, so don't make __cwd_buf_size larger than 256
 ; without checking the other sources.
+;
+; __cwd is initialized by a platform specific function named "initcwd" called
+; by the constructor defined in this module.
+;
 
        	.export		__cwd
         .export         __cwd_buf_size
+        .constructor    cwd_init
+
+        .import         initcwd
 
         __cwd_buf_size  = 64
+        cwd_init        := initcwd
 
 .bss
 

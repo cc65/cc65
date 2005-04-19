ee81f9a68679855caee413d6f81437ff57c2f933
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2005-04-19 11:28:11 +0000

    Make the size of _cwd FILENAME_MAX chars
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3465 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/_cwd.s
+++ libsrc/common/_cwd.s
@@ -2,25 +2,29 @@
 ; Ullrich von Bassewitz, 2003-08-12, 2005-04-16
 ;
 ; Place to store the current working directory.
-; NOTE: Some of the code working with directories is not able to handle
-; strings longer than 255 chars, so don't make __cwd_buf_size larger than 256
-; without checking the other sources.
 ;
 ; __cwd is initialized by a platform specific function named "initcwd" called
 ; by the constructor defined in this module.
 ;
 
        	.export		__cwd
-        .export         __cwd_buf_size
         .constructor    cwd_init
 
         .import         initcwd
 
-        __cwd_buf_size  = 64
+        .include        "stdio.inc"
+
+
         cwd_init        := initcwd
 
 .bss
 
-__cwd:  .res	__cwd_buf_size
+__cwd:  .res	FILENAME_MAX
 
+
+; NOTE: Some of the code working with directories is not able to handle
+; strings longer than 255 chars, so don't make __cwd larger than 256 without
+; checking the other sources.
+
+        .assert FILENAME_MAX < 256, error, "FILENAME_MAX must not be > 255"
 

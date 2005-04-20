f3ac2be07df41a5bf3c4fd503a0baebae2b03f51
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2005-04-20 16:52:10 +0000

    Cosmetic changes from Oliver Schmidt
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3477 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/apple2/color.s
+++ libsrc/apple2/color.s
@@ -6,13 +6,11 @@
 ; unsigned char __fastcall__ bordercolor (unsigned char color);
 ;
 
- 	.export		_textcolor, _bgcolor, _bordercolor
-	.import	       	return0, return1
+        .export		_textcolor, _bgcolor, _bordercolor
+        .import	       	return0, return1
 
-_textcolor	= return1
+_textcolor      := return1
 
-_bgcolor	= return0
+_bgcolor        := return0
 
-_bordercolor	= return0
-
-
+_bordercolor    := return0
--- libsrc/apple2/mli.s
+++ libsrc/apple2/mli.s
@@ -4,32 +4,32 @@
 ; Apple ProDOS 8 MLI
 ;
 
-        .import		__dos_type
+        .import         __dos_type
 
-        .include	"mli.inc"
+        .include        "mli.inc"
 
         .bss
 
-mliparam:       .tag	MLI
+mliparam:.tag   MLI
 
         .data
 
 callmli:
         ; Store parameters
-        sta	call
-        stx	mliparam
+        sta     call
+        stx     mliparam
 
         ; Check for ProDOS 8
-        lda	__dos_type
-        beq	oserr
+        lda     __dos_type
+        beq     oserr
 
         ; Call MLI and return
-        jsr	ENTRY
-call:   .byte	$00
-        .word	mliparam
+        jsr     ENTRY
+call:   .byte   $00
+        .addr   mliparam
         rts
 
         ; Load oserror code and return
-oserr:  lda	#$01		; "Invalid MLI function code number"
+oserr:  lda     #$01		; "Invalid MLI function code number"
         sec
         rts

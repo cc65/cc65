a63e74f6915a4250db7fc90958c8d88f0cc5281a
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2005-11-20 12:52:53 +0000

    Insert correct .setcpu line for HuC6280
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3651 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/cc65/codegen.c
+++ src/cc65/codegen.c
@@ -161,6 +161,7 @@ void g_preamble (void)
         case CPU_65SC02:    AddTextLine ("\t.setcpu\t\t\"65SC02\"");    break;
         case CPU_65C02:     AddTextLine ("\t.setcpu\t\t\"65C02\"");     break;
         case CPU_65816:     AddTextLine ("\t.setcpu\t\t\"65816\"");     break;
+        case CPU_HUC6280:   AddTextLine ("\t.setcpu\t\t\"HUC6280\"");   break;
         default:            Internal ("Unknown CPU: %d", CPU);
     }
 

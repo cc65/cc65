f981dd35adeacd982767466550473127d9277582
cpg <cpg@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2006-01-05 15:11:33 +0000

    fix INSTALL return value (Stefan Haubenthal)
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3695 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/atari/atari-stdjoy.s
+++ libsrc/atari/atari-stdjoy.s
@@ -64,8 +64,8 @@ JOY_COUNT	= 4		; Number of joysticks we support
 ;
 
 INSTALL:
-	lda	#<JOY_ERR_OK
-	ldx	#>JOY_ERR_OK
+	lda	#JOY_ERR_OK
+	ldx	#0
 ;	rts			; Run into UNINSTALL instead
 
 ; ------------------------------------------------------------------------

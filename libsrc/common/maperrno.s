545df804bb097b28363bc03c29e6075de93806d0
cuz <cuz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2008-04-15 06:55:35 +0000

    Renamed setoserror to seterrnofromoserror to avoid clashes.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3838 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/maperrno.s
+++ libsrc/common/maperrno.s
@@ -16,13 +16,13 @@
 __maperrno:
 	lda	__oserror		; Get the error code
        	beq    	L9			; Jump if no error
-setoserror:
+seterrnofromoserror:     
 	ldx	#$00			; Clear error
 	stx	__oserror
 	jsr	__osmaperrno		; Map the code
        	sta    	__errno
 	stx	__errno+1
-L9:	rts       
+L9:	rts
 
 
 

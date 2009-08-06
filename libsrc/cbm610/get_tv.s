f8d3fccc01ef5378ee652a3431f7d9b676c443e4
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2009-08-06 20:24:41 +0000

    get_tv() implementation for the CBM610 by Stefan Haubenthal.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@3998 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/cbm610/get_tv.s
@@ -0,0 +1,20 @@
+;
+; Stefan Haubenthal, 2009-08-02
+;
+; unsigned char __fastcall__ get_tv (void);
+; /* Return the video mode the machine is using */
+;
+
+	.include	"get_tv.inc"
+
+
+;--------------------------------------------------------------------------
+; _get_tv
+
+.proc	_get_tv
+
+       	lda    	#<TV::OTHER	; CRTC
+	ldx	#>TV::OTHER
+	rts
+
+.endproc

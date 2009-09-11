569b8d0e77d46d65168b97cf74ce0670c15971ce
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2009-09-11 18:47:33 +0000

    Separated tgi_load and tgi_load_driver.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4152 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/tgi/tgi_load_driver.c
+++ libsrc/tgi/tgi_load_driver.c
@@ -1,6 +1,6 @@
 /*****************************************************************************/
 /*                                                                           */
-/*                                tgi_load.c                                 */
+/*                             tgi_load_driver.c                             */
 /*                                                                           */
 /*                       Loader module for TGI drivers                       */
 /*                                                                           */
@@ -33,6 +33,9 @@
 
 
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <modload.h>
 #include <tgi.h>
 #include <tgi/tgi-kernel.h>
 
@@ -44,20 +47,42 @@
 
 
 
-void __fastcall__ tgi_load (unsigned char mode)
-/* Install the matching driver for the given mode. Will just load the driver
- * and check if loading was successul. Will not switch to gaphics mode.
+void __fastcall__ tgi_load_driver (const char* name)
+/* Install the given driver. This function is identical to tgi_load with the
+ * only difference that the name of the driver is specified explicitly. You
+ * should NOT use this function in most cases, use tgi_load() instead.
  */
 {
-    const char* name = tgi_map_mode (mode);
-    if (name == 0) {
-        /* No driver for this mode */
-        tgi_error = TGI_ERR_NO_DRIVER;
-    } else {
-        /* Load the driver */
-        tgi_load_driver (name);
+    static struct mod_ctrl ctrl = {
+        read            /* Read from disk */
+    };
+    unsigned Res;
+
+    /* Check if we do already have a driver loaded. If so, remove it. */
+    if (tgi_drv != 0) {
+        tgi_unload ();
     }
+
+    /* Now open the file */
+    ctrl.callerdata = open (name, O_RDONLY);
+    if (ctrl.callerdata >= 0) {
+
+        /* Load the module */
+        Res = mod_load (&ctrl);
+
+        /* Close the input file */
+        close (ctrl.callerdata);
+
+        /* Check the return code */
+        if (Res == MLOAD_OK) {
+
+            /* Check the driver signature, install the driver */
+            tgi_install (ctrl.module);
+            return;
+
+        }
+    }
+
+    /* Error loading the driver */
+    tgi_error = TGI_ERR_CANNOT_LOAD;
 }
-
-
-

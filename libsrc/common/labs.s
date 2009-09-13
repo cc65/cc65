4b84d9589123ff4b8d6346028abdf35ced90343e
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2009-09-13 18:06:43 +0000

    labs() is also imaxabs().
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4167 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/labs.s
+++ libsrc/common/labs.s
@@ -1,14 +1,18 @@
 ;
 ; Ullrich von Bassewitz, 17.06.1998
 ;
-; long labs (long x);
+; long __fastcall__ labs (long val);
+; intmax_t __fastcall imaxabs (intmax_t val);
 ;
 
-	.export		_labs
-	.import		negeax, tsteax
+  	.export		_labs, _imaxabs
+  	.import		negeax
 	.importzp	sreg
 
-_labs: 	ldy	sreg+1		; test hi byte
+
+_labs:
+_imaxabs:
+        ldy	sreg+1		; test hi byte
   	bpl	L1
 	jmp	negeax 		; Negate if negative
 L1:    	rts

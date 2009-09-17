75068a652891a3ee32fa4025c10557501d44f950
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2009-09-17 18:37:42 +0000

    New strtoimax and strtoumax standard functions as aliases for strtol/strtoul.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4182 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/common/strtoimax.s
@@ -0,0 +1,9 @@
+;
+; Ullrich von Bassewitz, 2009-09-17
+;
+; intmax_t __fastcall__ strtoimax (const char* nptr, char** endptr, int base);
+;
+
+        .import         _strtol
+      	.export		_strtoimax = _strtol
+
---
+++ libsrc/common/strtoumax.s
@@ -0,0 +1,9 @@
+;
+; Ullrich von Bassewitz, 2009-09-17
+;
+; uintmax_t __fastcall__ strtoumax (const char* nptr, char** endptr, int base);
+;
+
+        .import         _strtoul
+      	.export		_strtoumax = _strtoul
+

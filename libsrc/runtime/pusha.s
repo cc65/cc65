54a8a778eba81f033d98949c2ffb16fb62448bd8
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2009-10-20 08:25:58 +0000

    Remove 65C02 code, because in this case, the value of Y on return is always zero, which can be used by the optimizer.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4384 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/runtime/pusha.s
+++ libsrc/runtime/pusha.s
@@ -18,12 +18,8 @@ pushaysp:
 pusha:	ldy	sp              ; (3)
        	beq	@L1             ; (6)
  	dec	sp              ; (11)
-.if (.cpu .bitand CPU_ISET_65SC02)
-	sta	(sp)
-.else
     	ldy	#0              ; (13)
     	sta	(sp),y          ; (19)
-.endif
     	rts                     ; (25)
 
 @L1:	dec	sp+1            ; (11)

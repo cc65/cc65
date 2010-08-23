5aff0764a13dc6fc8dd612ac59f3ba25e4791f16
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2010-08-23 18:18:53 +0000

    Allow initialization of variables declared with "extern".
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4816 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/cc65/compile.c
+++ src/cc65/compile.c
@@ -138,15 +138,20 @@ static void Parse (void)
 	    }
 
             /* Check if we must reserve storage for the variable. We do this,
-             * if it is not a typedef or function, if we don't had a storage
-             * class given ("int i") or if the storage class is explicitly
-             * specified as static. This means that "extern int i" will not
-             * get storage allocated.
+             * 
+             *   - if it is not a typedef or function, 
+             *   - if we don't had a storage class given ("int i")
+             *   - if the storage class is explicitly specified as static,
+             *   - or if there is an initialization.
+             *
+             * This means that "extern int i;" will not get storage allocated.
              */
 	    if ((Decl.StorageClass & SC_FUNC) != SC_FUNC          &&
                 (Decl.StorageClass & SC_TYPEDEF) != SC_TYPEDEF    &&
-                ((Spec.Flags & DS_DEF_STORAGE) != 0  ||
-                 (Decl.StorageClass & (SC_STATIC | SC_EXTERN)) == SC_STATIC)) {
+                ((Spec.Flags & DS_DEF_STORAGE) != 0         ||
+                 (Decl.StorageClass & SC_STATIC) != 0       ||
+                 ((Decl.StorageClass & SC_EXTERN) != 0 && 
+                  CurTok.Tok == TOK_ASSIGN))) {
 
                 /* We will allocate storage */
 		Decl.StorageClass |= SC_STORAGE | SC_DEF;

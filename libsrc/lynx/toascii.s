0d5267fc1a2491e2dd067f6be7ee0741877929ce
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2010-12-19 11:49:58 +0000

    Added an implementation of toascii() for the Lynx, contributed by Karri Kaksonen.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4887 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/lynx/toascii.s
@@ -0,0 +1,16 @@
+;
+; unsigned char __fastcall__ toascii (unsigned char c);
+; /* Convert a target specific character to ascii */
+;
+
+.export _toascii
+
+.proc _toascii
+
+; X must be zero on return
+	ldx     #0
+
+; Done!
+	rts
+
+.endproc

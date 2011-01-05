d60c627cb62de7e55fe6abeb22aada9bc60689d0
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2011-01-05 10:47:32 +0000

    When replaying a token list, be sure to zero terminate the string attribute of a token.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@4900 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/ca65/toklist.c
+++ src/ca65/toklist.c
@@ -6,7 +6,7 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 1998-2008, Ullrich von Bassewitz                                      */
+/* (C) 1998-2010, Ullrich von Bassewitz                                      */
 /*                Roemerstrasse 52                                           */
 /*                D-70794 Filderstadt                                        */
 /* EMail:         uz@cc65.org                                                */
@@ -93,6 +93,7 @@ void TokSet (TokNode* T)
     WS   = T->WS;
     IVal = T->IVal;
     SB_Copy (&SVal, &T->SVal);
+    SB_Terminate (&SVal);
 }
 
 

9bf3446060fe1200502a11c3b8eed49f301b9394
ol.sc <ol.sc@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2011-04-30 13:12:53 +0000

    A first small step towards aspect ratio awareness.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5003 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- samples/tgidemo.c
+++ samples/tgidemo.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <modload.h>
 #include <tgi.h>
+#include <tgi/tgi-kernel.h>
 
 
 
@@ -70,7 +71,7 @@ static void DoCircles (void)
         tgi_line (0, MaxY, MaxX, 0);
         tgi_setcolor (Color);
         for (I = 10; I < 240; I += 10) {
-            tgi_circle (X, Y, I);
+            tgi_ellipse (X, Y, I, tgi_imulround (I, tgi_aspectratio));
         }
 	Color = Color == COLOR_FORE ? COLOR_BACK : COLOR_FORE;
     }

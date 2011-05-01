06fddaf11f7d19bc8ae6fe61b9e6ec6bea3e25f0
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2011-05-01 21:39:04 +0000

    Improved the stub for __osmaperrno somewhat (Stefan Haubenthal).
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5013 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/atmos/oserror.s
+++ libsrc/atmos/oserror.s
@@ -1,11 +1,17 @@
 ;
-; Stefan Haubenthal, 2011-04-10
+; Stefan Haubenthal, 2011-04-18
 ;
 ; int __fastcall__ _osmaperrno (unsigned char oserror);
 ; /* Map a system specific error into a system independent code */
 ;
 
+	.include	"errno.inc"
 	.export		__osmaperrno
-	.import		return0
 
-__osmaperrno	= return0
+.proc	__osmaperrno
+
+	lda	#<EUNKNOWN
+	ldx	#>EUNKNOWN
+	rts
+
+.endproc

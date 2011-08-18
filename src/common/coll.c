bd233ac916a576a957606ce8ad7815500839fb57
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2011-08-18 14:13:14 +0000

    Decrease the minimum size of a collection.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5211 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- src/common/coll.c
+++ src/common/coll.c
@@ -6,7 +6,7 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 2000-2010, Ullrich von Bassewitz                                      */
+/* (C) 2000-2011, Ullrich von Bassewitz                                      */
 /*                Roemerstrasse 52                                           */
 /*                D-70794 Filderstadt                                        */
 /* EMail:         uz@cc65.org                                                */
@@ -140,7 +140,7 @@ void CollInsert (Collection* C, void* Item, unsigned Index)
     /* Grow the array if necessary */
     if (C->Count >= C->Size) {
      	/* Must grow */
-        CollGrow (C, (C->Size == 0)? 8 : C->Size * 2);
+        CollGrow (C, (C->Size == 0)? 4 : C->Size * 2);
     }
 
     /* Move the existing elements if needed */

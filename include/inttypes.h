eef45199d52c3d88114bd9f310b221378b72176f
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2011-11-05 19:49:34 +0000

    Remove ldiv_t - it should only be in stdlib.h. Reported by Marc Rintsch.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5302 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- include/inttypes.h
+++ include/inttypes.h
@@ -6,7 +6,7 @@
 /*                                                                           */
 /*                                                                           */
 /*                                                                           */
-/* (C) 2002-2009, Ullrich von Bassewitz                                      */
+/* (C) 2002-2011, Ullrich von Bassewitz                                      */
 /*                Roemerstrasse 52                                           */
 /*                D-70794 Filderstadt                                        */
 /* EMail:         uz@cc65.org                                                */
@@ -50,13 +50,6 @@
 #include <stdint.h>
 #endif
 
-
-
-/* Return type of the imaxdiv function (which currently doesn't exist) */
-typedef struct {
-    intmax_t rem;
-    intmax_t quot;
-} ldiv_t;
 
 
 /* Standard functions */

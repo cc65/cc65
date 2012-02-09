bf6b0f1e4b9d892e5f1172074f72b38421ab5882
ol.sc <ol.sc@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-02-09 10:03:54 +0000

    Initial Apple GEOS build.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5512 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/geos-apple/disk/enterturbo.s
@@ -0,0 +1,11 @@
+;
+; Oliver Schmidt (ol.sc@web.de)
+;
+; 2012-02-09
+
+; void EnterTurbo (void);
+
+	    .export _EnterTurbo
+	    .import return0
+
+_EnterTurbo	= return0
---
+++ libsrc/geos-apple/disk/purgeturbo.s
@@ -0,0 +1,11 @@
+;
+; Oliver Schmidt (ol.sc@web.de)
+;
+; 2012-02-09
+
+; void PurgeTurbo (void);
+
+	    .export _PurgeTurbo
+	    .import return0
+
+_PurgeTurbo	= return0

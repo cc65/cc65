cc5fd1dd72f74954c772814bc7f5c644497497c0
ol.sc <ol.sc@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-06-10 17:52:23 +0000

    Replaced (broken) assert() with #error and added a source bitmap.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5700 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- samples/geos/bitmap-demo.c
+++ samples/geos/bitmap-demo.c
@@ -4,22 +4,22 @@
  * 2012-06-10, Oliver Schmidt (ol.sc@web.de)
  *
  * To create bitmap.c use the sp65 sprite and bitmap utility:
- * sp65 -r <input>.pcx -c geos-bitmap -w bitmap.c,ident=bitmap
+ * sp65 -r logo.pcx -c geos-bitmap -w bitmap.c,ident=bitmap
  *
  */
 
 
-#include <assert.h>
 #include <conio.h>
 #include <geos.h>
 
 
 #include "bitmap.c"
-
-//assert(bitmap_COLORS  == 2);
-//assert(bitmap_WIDTH%8 == 0);
-//assert(bitmap_WIDTH   <= SC_PIX_WIDTH);
-//assert(bitmap_HEIGHT  <= SC_PIX_HEIGHT);
+#if (!(bitmap_COLORS  == 2            && \
+       bitmap_WIDTH%8 == 0            && \
+       bitmap_WIDTH   <= SC_PIX_WIDTH && \
+       bitmap_HEIGHT  <= SC_PIX_HEIGHT))
+#error Incompatible Bitmap
+#endif
 
 
 struct iconpic picture = {(char*)bitmap, 0, 0, bitmap_WIDTH/8, bitmap_HEIGHT};

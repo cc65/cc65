bfd0f586860607166ff1a51c1e6c0beb688bb272
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-06-10 18:25:22 +0000

    Shortened the code.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5701 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/common/fdopen.c
+++ libsrc/common/fdopen.c
@@ -21,17 +21,17 @@
 
 FILE* __fastcall__ fdopen (int handle, const char* /*mode*/)
 {
-    FILE* f;
+    register FILE* f;
 
     /* Find a free file slot */
-    if (!(f = _fdesc ())) {
+    if ((f = _fdesc ())) {
+        /* Insert the handle, and return the descriptor */
+        f->f_fd    = handle;
+        f->f_flags = _FOPEN;
+    } else {
        	/* No slots */
-       	return (FILE*) _seterrno (EMFILE);      /* Too many files */
+       	_seterrno (EMFILE);      /* Too many files */
     }
-
-    /* Insert the handle, and return the descriptor */
-    f->f_fd    = handle;
-    f->f_flags = _FOPEN;
 
     /* Return the file descriptor */
     return f;

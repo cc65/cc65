49542644df585ec3b9c70f35fd2d1365146fab9c
uz <uz@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-07-01 11:30:41 +0000

    Added missing function cbm_k_getin, which was in cbm.h but an implementation was missing.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5747 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/cbm/c_k_getin.s
@@ -0,0 +1,14 @@
+;
+; Ullrich von Bassewitz, 2012-07-01
+;
+; unsigned char cbm_k_getin (void);
+;
+
+       	.export	       	_cbm_k_getin
+        .import         GETIN
+
+
+_cbm_k_getin:
+	jsr    	GETIN
+	ldx	#0
+	rts

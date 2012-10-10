300817702fb454207a241a20c8d940b04b280ff8
ol.sc <ol.sc@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-10-10 19:32:30 +0000

    initcwd will be called from the upcoming __syschdir so it has to be moved out of the INIT segment - and so it doesn't make sense anymore to keep devicestr as separate module anymore as the integration with initcwd allows to omit the jmp from the latter to the former.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5844 b7a2c559-68d2-44c3-8de9-860c34a00d81

--- libsrc/cbm/initcwd.s
+++ libsrc/cbm/initcwd.s
@@ -4,19 +4,38 @@
 ; CBM _curunit to _cwd
 ;
 
-	.export		initcwd
-	.import		__curunit, __cwd, devicestr
-	.importzp	ptr2
+	.export		initcwd, devicestr
+	.import		__curunit, __cwd
+	.import		pusha0, tosudiva0
+	.importzp	sreg, ptr1, ptr2
 
-	.segment	"INIT"
+	.macpack	generic
 
-.proc	initcwd
-
+initcwd:
 	lda	#<__cwd
 	ldx	#>__cwd
 	sta	ptr2
 	stx	ptr2+1
 	lda	__curunit
-	jmp	devicestr
+	; Fall through
 
-.endproc
+;------------------------------------------------------------------------------
+; Convert unit number in A into string representation pointed to by ptr2.
+
+devicestr:
+	jsr	pusha0
+	lda	#10
+	jsr	tosudiva0
+	ldy	#0
+	lda	sreg
+	beq	:+		; >=10
+	add	#'0'
+	sta	(ptr2),y
+	iny
+:	lda	ptr1		; rem
+	add	#'0'
+	sta	(ptr2),y
+	iny
+	lda	#0
+	sta	(ptr2),y
+	rts

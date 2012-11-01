dbc66c247a018b27e7de274972f71941080fe295
ol.sc <ol.sc@b7a2c559-68d2-44c3-8de9-860c34a00d81>
2012-11-01 15:58:27 +0000

    Added missing standard driver definitions.
    git-svn-id: svn://svn.cc65.org/cc65/trunk@5890 b7a2c559-68d2-44c3-8de9-860c34a00d81

---
+++ libsrc/atmos/tgi_stddrv.s
@@ -0,0 +1,13 @@
+;
+; Name of the standard tgi driver
+;
+; Oliver Schmidt, 2012-11-01
+;
+; const char tgi_stddrv[];
+;
+
+        .export	_tgi_stddrv
+
+.rodata
+
+_tgi_stddrv:	.asciiz	"atmos-240-200-2.tgi"

39a877175bdb6e65e95a9a9a0c9c0cced8eb41a8
Oliver Schmidt <ol.sc@web.de>
2013-05-02 15:39:47 +0300

    Added reference to unused variable to avoid GCC 4.6 warning.

--- src/sp65/koala.c
+++ src/sp65/koala.c
@@ -94,6 +94,9 @@ StrBuf* GenKoala (const Bitmap* B, const Collection* A attribute ((unused)))
     /* Add $4400 as load address */
     SB_AppendChar (D, 0x00);
     SB_AppendChar (D, 0x44);
+    
+    /* TODO: The actual work ;-) */
+    (void)Screen;
 
     /* Return the converted bitmap */
     return D;

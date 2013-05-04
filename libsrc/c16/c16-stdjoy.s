008b4c4e1d952cc71b082f121b7e6d3d9e43132c
Oliver Schmidt <ol.sc@web.de>
2013-05-04 22:10:48 +0200

    Replaced whole bunch for Makefiles with a single generic Makefile.
    - No complex shell logic.
    - "Source file shadowing" for all targets via vpath.
    - Dependency handling.
    - True incremental build.
    - Don't write into source directories.
    - Easy cleanup by just removing 'wrk'.

--- libsrc/c16/c16-stdjoy.s
+++
@@ -1,9 +0,0 @@
-;
-; Standard joystick driver for the C16. May be used multiple times when linked
-; to the statically application.
-;
-; Ullrich von Bassewitz, 2002-12-21
-;
-
-	.include	"../plus4/plus4-stdjoy.s"
-

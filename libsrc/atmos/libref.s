ae409e497851d1bc5bdb7cbec853bdeddf6f8750
Oliver Schmidt <ol.sc@web.de>
2013-06-01 11:03:14 +0200

    Added library reference ser_libref to SER interface.

--- libsrc/atmos/libref.s
+++ libsrc/atmos/libref.s
@@ -2,7 +2,8 @@
 ; Oliver Schmidt, 2013-05-31
 ;
 
-        .export         joy_libref
+        .export         joy_libref, ser_libref
         .import         _exit
 
 joy_libref      := _exit
+ser_libref      := _exit
--- libsrc/cbm610/libref.s
+++ libsrc/cbm610/libref.s
@@ -2,7 +2,8 @@
 ; Oliver Schmidt, 2013-05-31
 ;
 
-        .export         em_libref
+        .export         em_libref, ser_libref
         .import         _exit
 
 em_libref       := _exit
+ser_libref      := _exit

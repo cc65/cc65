a5dfce7c416b587b456286c11feaf4ac139feb67
Christian Groessler <chris@groessler.org>
2014-01-03 23:40:22 +0100

    Add "joystick mouse" driver and default mouse callback routine (not using P/M graphics).

--- libsrc/atari/libref.s
+++ libsrc/atari/libref.s
@@ -2,12 +2,13 @@
 ; Oliver Schmidt, 2013-05-31
 ;
 
-        .export         em_libref, joy_libref, tgi_libref, ser_libref
+        .export         em_libref, joy_libref, tgi_libref, ser_libref, mouse_libref
         .import         _exit
         .import         atari_ser_libref
 
 em_libref       := _exit
 joy_libref      := _exit
+mouse_libref    := _exit
 ser_libref      := atari_ser_libref
 .ifdef __ATARIXL__
         .import CIO_handler

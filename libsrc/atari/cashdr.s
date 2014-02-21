81fe7a38051030924a55ff278469a70c7e5b43c0
Christian Groessler <chris@groessler.org>
2014-02-21 22:43:44 +0100

    cleanups; split 'header' and 'init' part into two source files

--- libsrc/atari/cashdr.s
+++ libsrc/atari/cashdr.s
@@ -1,40 +1,37 @@
 ;
 ; Cassette boot file header
 ;
-; Christian Groessler, chris@groessler.org, 2013
+; Christian Groessler, chris@groessler.org, 2014
 ;
+
+;DEBUG   = 1
 
 .ifndef __ATARIXL__
 
-	.include "atari.inc"
+        .include "atari.inc"
 
-	.import	__BSS_RUN__, __STARTADDRESS__, start
-	.export	_cas_init
+        .import __BSS_RUN__, __STARTADDRESS__, _cas_init
+        .export _cas_hdr
+
+.assert ((__BSS_RUN__ - __STARTADDRESS__ + 127) / 128) < $101, error, "File to big to load from cassette"
+
+
+; for a description of the cassette header, see De Re Atari, appendix C
 
 .segment        "CASHDR"
 
-	.byte	0	; ignored
-	.byte	<((__BSS_RUN__ - __STARTADDRESS__ + 127 + 6) / 128)
-	.word	__STARTADDRESS__
-	.word	_cas_init
+_cas_hdr:
+        .byte   0                       ; ignored
+        .byte   <((__BSS_RUN__ - __STARTADDRESS__ + 127) / 128)         ; # of 128-byte records to read
+        .word   __STARTADDRESS__        ; load address
+        .word   _cas_init               ; init address
 
-.segment	"CASINIT"
+.ifdef  DEBUG
+        lda     #33
+        ldy     #80
+        sta     (SAVMSC),y
+.endif
+        clc
+        rts
 
-	lda	#33
-	ldy	#80
-	sta	(SAVMSC),y
-	clc
-	rts
-
-_cas_init:
-	lda	#34
-	ldy	#81
-	sta	(SAVMSC),y
-
-	lda	#<start
-	sta	DOSVEC
-	lda	#>start
-	sta	DOSVEC+1
-	rts
-
-.endif	; .ifdef __ATARIXL__
+.endif  ; .ifdef __ATARIXL__

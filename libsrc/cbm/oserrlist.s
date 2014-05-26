3d6f54b7a6b6497404f8931e71ad5ec488e78a3a
Greg King <gregdk@users.sf.net>
2014-05-26 09:03:43 -0400

    Updated the CBM error tables.
    * Added more mappings between DOS and C codes.
    * Changed generic error messages into specific ones.

--- libsrc/cbm/oserrlist.s
+++ libsrc/cbm/oserrlist.s
@@ -1,15 +1,16 @@
 ;
-; Ullrich von Bassewitz, 18.07.2002
+; 2002-07-18, Ullrich von Bassewitz
+; 2014-05-26, Greg King
 ;
-; Defines the platform specific error list.
+; Defines the platform-specific error list.
 ;
-; The table is built as a list of entries
+; The table is built as a list of entries:
 ;
 ;       .byte   entrylen
 ;       .byte   errorcode
 ;       .asciiz errormsg
 ;
-; and terminated by an entry with length zero that is returned if the
+; and, terminated by an entry with length zero that is returned if the
 ; error code could not be found.
 ;
 
@@ -49,22 +50,22 @@ __sys_oserrlist:
         sys_oserr_entry          7, "File not output"
         sys_oserr_entry          8, "Filename missing"
         sys_oserr_entry          9, "Illegal device"
-        sys_oserr_entry         20, "Read error"
-        sys_oserr_entry         21, "Read error"
-        sys_oserr_entry         22, "Read error"
-        sys_oserr_entry         23, "Read error"
-        sys_oserr_entry         24, "Read error"
-        sys_oserr_entry         25, "Write error"
+        sys_oserr_entry         20, "No sector header"
+        sys_oserr_entry         21, "No sync mark"
+        sys_oserr_entry         22, "No sector data"
+        sys_oserr_entry         23, "Checksum error"
+        sys_oserr_entry         24, "Decode error"
+        sys_oserr_entry         25, "Verify error"
         sys_oserr_entry         26, "Write protect on"
-        sys_oserr_entry         27, "Read error"
-        sys_oserr_entry         28, "Write error"
+        sys_oserr_entry         27, "Checksum error"
+        sys_oserr_entry         28, "Write overrun"
         sys_oserr_entry         29, "Disk ID mismatch"
-        sys_oserr_entry         30, "Syntax error"
-        sys_oserr_entry         31, "Syntax error"
-        sys_oserr_entry         32, "Syntax error"
-        sys_oserr_entry         33, "Syntax error (invalid file name)"
-        sys_oserr_entry         34, "Syntax error (no file given)"
-        sys_oserr_entry         39, "Syntax error"
+        sys_oserr_entry         30, "Command not recognized"
+        sys_oserr_entry         31, "Command not implemented"
+        sys_oserr_entry         32, "Command too long"
+        sys_oserr_entry         33, "Invalid write filename"
+        sys_oserr_entry         34, "No file given"
+        sys_oserr_entry         39, "File not found"
         sys_oserr_entry         50, "Record not present"
         sys_oserr_entry         51, "Overflow in record"
         sys_oserr_entry         52, "File too large"
@@ -77,10 +78,8 @@ __sys_oserrlist:
         sys_oserr_entry         66, "Illegal track or sector"
         sys_oserr_entry         67, "Illegal system track or sector"
         sys_oserr_entry         70, "No channel"
-        sys_oserr_entry         71, "Directory error"
+        sys_oserr_entry         71, "BAM error"
         sys_oserr_entry         72, "Disk full"
         sys_oserr_entry         73, "DOS version mismatch"
         sys_oserr_entry         74, "Drive not ready"
         sys_oserr_sentinel      "Unknown error"
-
-

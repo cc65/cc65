14958949c27d38dc69e5e228298d9ad5d9f906e3
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2014-11-16 21:13:18 +0100

    Direct use of monitor function instead of subroutine call.

--- libsrc/c1p/cgetc.s
+++ libsrc/c1p/cgetc.s
@@ -1,11 +1,8 @@
 ;
 ; char cgetc (void);
 ;
-
         .export         _cgetc
         .include        "c1p.inc"
 
-; Use INPUT routine from 65V PROM MONITOR
-_cgetc:
-        jsr $FEED
-        rts
+; Direct use of input routine from 65V PROM MONITOR
+_cgetc = INPUTC

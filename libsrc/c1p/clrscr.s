6dc8621fa537901bc9b19731ab3cb70780ff71b0
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2014-11-29 20:50:48 +0100

    Set cursor in top left corner in clrscr() as documented in conio.h

--- libsrc/c1p/clrscr.s
+++ libsrc/c1p/clrscr.s
@@ -2,6 +2,8 @@
 ; void clrscr (void);
 ;
         .export         _clrscr
+        .import         plot
+        .importzp       CURS_X, CURS_Y
         .include        "c1p.inc"
 
 ; Adapted from the Challenger Character Graphics
@@ -10,16 +12,20 @@
 BANKS = VIDEORAMSIZE / $100
 
 _clrscr:
-        lda #$20 ;' '
-		ldy #BANKS
-		ldx #$00
+        lda       #$20 ;' '
+		ldy       #BANKS
+		ldx       #$00
 staloc:
-		sta SCRNBASE,X
+		sta       SCRNBASE,X
 		inx
-		bne staloc
-		inc staloc+2
+		bne       staloc
+		inc       staloc+2
 		dey
-		bne staloc
-		lda #>(SCRNBASE) ; load high byte
-		sta staloc+2     ; restore base address
-		rts
+		bne       staloc
+		lda       #>(SCRNBASE); load high byte
+		sta       staloc+2    ; restore base address
+
+		lda       #$00        ; cursor in upper left corner
+        sta       CURS_X
+        sta       CURS_Y
+		jmp       plot        ; Set the cursor position

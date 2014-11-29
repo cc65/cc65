7dac57f60d46a06c8cb26f4e6fb625880ab44a7d
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2014-11-29 15:52:42 +0100

    Added gotox() and gotoy() implementations.

---
+++ libsrc/c1p/gotoy.s
@@ -0,0 +1,15 @@
+;
+; copied from CBM implementation
+; originally by:
+; Ullrich von Bassewitz, 0.08.1998
+;
+; void gotoy (unsigned char y);
+;
+
+        .export         _gotoy
+        .import         plot
+        .importzp       CURS_Y
+
+_gotoy: sta     CURS_Y          ; Set the new position
+        jmp     plot            ; And activate it
+

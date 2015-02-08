498fbb7ed729c58478c27068d021006471917487
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2015-02-08 17:35:40 +0100

    Fix formatting, replace tabs with spaces.

--- libsrc/osic1p/clrscr.s
+++ libsrc/osic1p/clrscr.s
@@ -13,19 +13,19 @@ BANKS = VIDEORAMSIZE / $100
 
 _clrscr:
         lda       #$20 ;' '
-		ldy       #BANKS
-		ldx       #$00
+        ldy       #BANKS
+        ldx       #$00
 staloc:
-		sta       SCRNBASE,X
-		inx
-		bne       staloc
-		inc       staloc+2
-		dey
-		bne       staloc
-		lda       #>(SCRNBASE); load high byte
-		sta       staloc+2    ; restore base address
+        sta       SCRNBASE,X
+        inx
+        bne       staloc
+        inc       staloc+2
+        dey
+        bne       staloc
+        lda       #>(SCRNBASE); load high byte
+        sta       staloc+2    ; restore base address
 
-		lda       #$00        ; cursor in upper left corner
+        lda       #$00        ; cursor in upper left corner
         sta       CURS_X
         sta       CURS_Y
-		jmp       plot        ; Set the cursor position
+        jmp       plot        ; Set the cursor position

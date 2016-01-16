7815e0e146cdf5c998d98396c029b31b66f39c32
Michaelangel007 <testcraft@sharedcraft.com>
2016-01-16 14:13:12 -0800

    Cleanup section indentation for readability

--- apple2/src2dsk.sh
+++ apple2/src2dsk.sh
@@ -33,47 +33,54 @@
 COPY=cp
 DEL=rm
 
-if [[ -z ${1} ]]; then
-    echo "ERROR: need an assembly source file (e.g. foo.s) to build from!"
-    exit 1
-fi
-
-if [ ! -f a2in ]; then
-    echo "WARNING: Missing a2tools 'a2in', attempting to build"
-    echo "INFO: Compiling 'a2tools' ..."
-    echo "  gcc -DUNIX a2tools.c -o a2in"
-    gcc -DUNIX a2tools.c -o a2in
-    if [[ -f a2in ]]; then
-        echo "... success!"
-    else
-        echo "ERROR: a2tools missing: 'a2in'"
-        echo " "
-        echo "The original tools can be found here:"
-        echo " * ftp://ftp.apple.asimov.net/pub/apple_II/utility/"
-        echo " * http://slackbuilds.org/repository/14.1/system/a2tools/"
-        echo "To download:"
-        echo "   curl -o a2tools.zip ftp://ftp.apple.asimov.net/pub/apple_II/utility/a2tools.zip"
-        echo "This repo. contains a copy but was unable to compile it."
+# Verify we have a .s file !
+    if [[ -z ${1} ]]; then
+        echo "ERROR: need an assembly source file (e.g. foo.s) to build from!"
         exit 1
     fi
-fi
 
+# Verify we can find toolchain
 # http://www.cc65.org/doc/ca65-2.html#ss2.2
-if [[ -z ${cc65dir} ]]; then
-    echo "INFO: 'cc65dir' not set, should point to directory containing 'ca65', 'ld65'"
-    cc65dir=../bin
-    echo "INFO: Trying '${cc65dir}' for assembler and linker"
-fi
+    if [[ -z ${cc65dir} ]]; then
+        echo "INFO: 'cc65dir' not set, should point to directory containing 'ca65', 'ld65'"
+        cc65dir=../bin
+        echo "INFO: Trying '${cc65dir}' for assembler and linker"
+    fi
 
-if [ ! -f ${cc65dir}/ca65 ]; then
-    echo "ERROR: Couldn't find assembler 'ca65' !"
-    exit 1
-fi
-if [ ! -f ${cc65dir}/ld65 ]; then
-    echo "ERROR: Couldn't find linker 'ld65' !"
-    exit 1
-fi
+# Assembler
+    if [ ! -f ${cc65dir}/ca65 ]; then
+        echo "ERROR: Couldn't find assembler 'ca65' !"
+        exit 1
+    fi
 
+# Linker
+    if [ ! -f ${cc65dir}/ld65 ]; then
+        echo "ERROR: Couldn't find linker 'ld65' !"
+        exit 1
+    fi
+
+# Verify we can find a2tools
+    if [ ! -f a2in ]; then
+        echo "WARNING: Missing a2tools 'a2in', attempting to build"
+        echo "INFO: Compiling 'a2tools' ..."
+        echo "  gcc -DUNIX a2tools.c -o a2in"
+        gcc -DUNIX a2tools.c -o a2in
+        if [[ -f a2in ]]; then
+            echo "... success!"
+        else
+            echo "ERROR: a2tools missing: 'a2in'"
+            echo " "
+            echo "The original tools can be found here:"
+            echo " * ftp://ftp.apple.asimov.net/pub/apple_II/utility/"
+            echo " * http://slackbuilds.org/repository/14.1/system/a2tools/"
+            echo "To download:"
+            echo "   curl -o a2tools.zip ftp://ftp.apple.asimov.net/pub/apple_II/utility/a2tools.zip"
+            echo "This repo. contains a copy but was unable to compile it."
+            exit 1
+        fi
+    fi
+
+# Filenames and extensions
     #http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
     # Get filename without path
     # Get filename without extension
@@ -87,21 +94,24 @@ fi
     ASM_FLAGS="--cpu 65c02"
     LNK_FLAGS="-C apple2bin.cfg"
 
+# Assemble & Link
     DEBUG=
     #DEBUG=echo
     ${DEBUG} ${DEL}                          ${BIN}   ${OBJ}
     ${DEBUG} ${cc65dir}/ca65 ${ASM_FLAGS}          -o ${OBJ} ${SRC}
     ${DEBUG} ${cc65dir}/ld65 ${LNK_FLAGS} -o ${BIN}   ${OBJ}
 
+# Copy to .DKS
     # We need to uppercase the file name for a DOS 3.3 DSK
     # The ${1,,} is a Bash 4.0 uppercase extension so we can't use that
     # Likewise, GNU sed 's/.*/\L&/g' doesn't work on OSX (BSD)
-        A2FILE=`echo "${FILE}" | awk '{print toupper($0)}'`
-        ${COPY}  empty.dsk ${FILE}.dsk
-        # If you want to keep an existing disk
-        # you will want to first remove the old version on .DSK
-        #${DEBUG} a2rm      ${FILE}.dsk ${A2FILE}
-        ${DEBUG} a2in -r b ${FILE}.dsk ${A2FILE} ${BIN}
+    A2FILE=`echo "${FILE}" | awk '{print toupper($0)}'`
+    ${COPY}  empty.dsk ${FILE}.dsk
+    # If you want to keep an existing disk
+    # you will want to first remove the old version on .DSK
+    #${DEBUG} a2rm      ${FILE}.dsk ${A2FILE}
+    ${DEBUG} a2in -r b ${FILE}.dsk ${A2FILE} ${BIN}
 
-echo "INFO: Created: ${FILE}.dsk"
+# Done!
+    echo "INFO: Created: ${FILE}.dsk"
 

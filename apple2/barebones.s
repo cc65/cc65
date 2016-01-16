67a35f186d2e714f9c94191c6f6c95e2471b2f33
Michaelangel007 <testcraft@sharedcraft.com>
2016-01-16 09:02:50 -0800

    Document and fix bogus linker range error

--- apple2/barebones.s
+++ apple2/barebones.s
@@ -1,10 +1,11 @@
-; Example showing how to assemble+link without any of the library crap
-
-; ca65 doesn't have a directive to set the high bit
+; Example showing how to assemble+link without any of the cc65 library crap
+; to produce a standalone Apple DOS3.3 binary barbones file
+;
+; ca65 doesn't have a directive to set the high bit on text
 ; as the original author didn't know anything about the Apple. :-/
 ; http://www.cc65.org/mailarchive/2003-05/2992.html
 ;
-; NOTE: There is no standard directive to set the high bit on ASCII text.
+; NOTE: There is no "standard" directive to set the high bit on ASCII text.
 ;
 ; * The assembler used in the redbook uses `msb on` and `msb off`
 ; * S-C Macro Assembler used
@@ -27,20 +28,32 @@
 
             COUT = $FDED
 
-            ; 4 byte header for DOS 3.3 files
-            .word $1000     ; define 2 bytes, must match org
-            .word __END - * ; define 2 bytes, total size in bytes
+; The operator '*' is buggy. This will generate a bogusBogus link error:
+; ld65: Error: Range error in module `link_bug.s', line 3
+;            __MAIN = $1000       ; Apple DOS 3.3 binary file 4 byte prefix header
+;            .word __MAIN         ; 2 byte BLAOD address
+;            .word __END - __MAIN ; 2 byte BLOAD size
+;
+; Solution 1 is to pad a dummy byte onto the end:
+;     __END:
+;     .asciiz ""
+;
+; Solution 2 is to not use the buggy '*' operator, replace with a real variable
+;     .wordwd __END - __MAIN
 
-            .org  $1000     ; .org must come after header else offsets are wrong
+            __MAIN = $1000       ; Apple DOS 3.3 binary file 4 byte prefix header
+            .word __MAIN         ; 2 byte blaod address
+            .word __END - __MAIN ; 2 byte bload size
+
+            .org  __MAIN         ; .org must come after header else offsets are wrong
             LDX    #0
             LDA    MSG,X    ; load initial char
-print:      ORA    #$80     ; work-around stupid macro bug *sigh*
+PRINTCHAR:  ORA    #$80     ; work-around stupid macro bug *sigh*
             JSR    COUT
             INX
             LDA    MSG,X
-            BNE    print
+            BNE    PRINTCHAR
             RTS
-
 MSG:        .asciiz "Hello, world!"
 
 __END:

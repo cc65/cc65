30c281d7c8e1d024f5efdb143671606e86066d0c
Michaelangel007 <testcraft@sharedcraft.com>
2016-01-16 16:22:50 -0800

    Use apple2text.inc file now

--- apple2/barebones.s
+++ apple2/barebones.s
@@ -1,32 +1,7 @@
 ; Example showing how to assemble+link without any of the cc65 library crap
 ; to produce a standalone Apple DOS3.3 binary barbones file
-;
-; ca65 doesn't have a directive to set the high bit on text
-; as the original author didn't know anything about the Apple. :-/
-; http://www.cc65.org/mailarchive/2003-05/2992.html
-;
-; NOTE: There is no "standard" directive to set the high bit on ASCII text.
-;
-; * The assembler used in the redbook uses `msb on` and `msb off`
-; * S-C Macro Assembler used
-;     .AS for normal ASCII, and
-;     .AT for normal ASCII but the last char has the high bit ON
-; * Merlin uses yet another variation:
-;     ASC 'Hello'   ; high bit is off
-;     ASC "Hello"   ; high bit is ON
-;   See the new Merlin-32 project
-;   http://brutaldeluxe.fr/products/crossdevtools/merlin/
-; 
-; "The 'nice' thing about standards, is that there are so many to pick from!"
-;
-; NOTE: If you use the buggy '*' operator (see note below) this macro won't
-; process the last 4 chars! Once we used a working "END-MAIN" we no longer
-; need to manually set the high bit when printing characters via `ORA #80`.
-.macro ASC text
-    .repeat .strlen(text), I
-    .byte   .strat(text, I) | $80
-    .endrep
-.endmacro
+
+            .include "apple2text.inc" ; AR, AS and AT macros
 
             COUT = $FDED
 
@@ -71,9 +46,14 @@ PRINTCHAR:  JSR    COUT
             JMP $3D0
 
 MSG:
-            ASC "Hello world, Apple"
-            .byte "!"           ; normal ASCII displayed as inverse
+            ; = Apple Text Demo =
+            ;AR "ABC"            ; NORMAL : APPLE high-bit text, last char is ASCII (FLASHING)
+            ;AS "Hello world"    ; NORMAL : APPLE high-bit text
+            ;AT "XYZ"            ; FLASH  : ASCII text, last char is APPLE high-bit (NORMAL)
+            ;AC "DEFM"           ; INVERSE: Force to be control chars0 $0..$1F (INVERSE)
+
+            AS "Hello world, Apple!"
             .byte $8D           ; CR with high-bit set
             .byte $00
-__END:
+__END = *
 

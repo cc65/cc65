4aa9a414c661a1e179500b02cf4f27dabf5b9272
Christian Groessler <chris@groessler.org>
2016-06-13 21:16:27 +0200

    Fix doesclrscrafterexit() function on atarixl target.
    On atarixl, the screen is always cleared, regardless of the running DOS.

--- libsrc/atari/is_cmdline_dos.s
+++ libsrc/atari/is_cmdline_dos.s
@@ -7,9 +7,14 @@
 ;
 
         .export  __is_cmdline_dos
-        .import  _doesclrscrafterexit
+        .import  __dos_type
+        .include "atari.inc"
 
 __is_cmdline_dos:
-        jsr     _doesclrscrafterexit    ; currently (unless a DOS behaving differently is popping up)
-        eor     #$01                    ; we can get by with the inverse of _doesclrscrafterexit
+        ldx     #0
+        lda     __dos_type
+        cmp     #MAX_DOS_WITH_CMDLINE + 1
+        txa
+        rol     a
+        eor     #$01
         rts

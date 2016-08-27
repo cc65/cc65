e786d1cf4911e3c82fcf2687f5798145f2334b17
alexthissen <athissen@killer-apps.nl>
2016-08-27 21:58:13 +0200

    Update exehdr.s
    Fix for memory bank 1 which should be zero for almost all cartridges for emulators to work correctly.

--- libsrc/lynx/exehdr.s
+++ libsrc/lynx/exehdr.s
@@ -12,7 +12,7 @@
         .segment "EXEHDR"
         .byte   'L','Y','N','X'                         ; magic
         .word   __BLOCKSIZE__                           ; bank 0 page size
-        .word   __BLOCKSIZE__                           ; bank 1 page size
+        .word   0                                       ; bank 1 page size
         .word   1                                       ; version number
         .asciiz "Cart name                      "       ; 32 bytes cart name
         .asciiz "Manufacturer   "                       ; 16 bytes manufacturer

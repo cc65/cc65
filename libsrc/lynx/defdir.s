ef7e9db1165b6c46ddc3eb9c4f7caf0b4c0ead28
Alex Thissen <alexthissen@hotmail.com>
2016-09-11 22:26:52 +0200

    Changed __BLOCKSIZE__ to __BANK0BLOCKSIZE__. Added __BANK1BLOCKSIZE__ which defaults to 0.

--- libsrc/lynx/defdir.s
+++ libsrc/lynx/defdir.s
@@ -8,7 +8,7 @@
         .import         __MAIN_START__
         .import         __CODE_SIZE__, __DATA_SIZE__, __RODATA_SIZE__
         .import         __STARTUP_SIZE__, __ONCE_SIZE__, __LOWCODE_SIZE__
-        .import         __BLOCKSIZE__
+        .import         __BANK0BLOCKSIZE__
         .export         __DEFDIR__: absolute = 1
 
 
@@ -18,12 +18,12 @@
 
 __DIRECTORY_START__:
 off0 = __STARTOFDIRECTORY__ + (__DIRECTORY_END__ - __DIRECTORY_START__)
-blocka = off0 / __BLOCKSIZE__
+blocka = off0 / __BANK0BLOCKSIZE__
 ; Entry 0 - first executable
-block0 = off0 / __BLOCKSIZE__
+block0 = off0 / __BANK0BLOCKSIZE__
 len0 = __STARTUP_SIZE__ + __ONCE_SIZE__ + __CODE_SIZE__ + __DATA_SIZE__ + __RODATA_SIZE__ + __LOWCODE_SIZE__
         .byte   <block0
-        .word   off0 & (__BLOCKSIZE__ - 1)
+        .word   off0 & (__BANK0BLOCKSIZE__ - 1)
         .byte   $88
         .word   __MAIN_START__
         .word   len0

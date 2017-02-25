7dc8d5dfaee1f2635b39426a7e7cdd3dcaf3313b
jede <jede@oric.org>
2017-02-25 09:47:51 +0100

    adding clrscr (for conio)

---
+++ libsrc/telestrat/clrscr.s
@@ -0,0 +1,25 @@
+    .export    _clrscr
+	
+    .import    addysp,popax
+	
+    .importzp  sp,tmp2,tmp3,tmp1
+	
+	
+    .include   "telestrat.inc"
+    .include   "errno.inc"
+    .include   "fcntl.inc"	
+
+.proc _clrscr
+    lda     #<SCREEN
+    sta     RES
+    lda     #>SCREEN
+    sta     RES+1
+
+    lda     #<(SCREEN+40*28)
+    sta     RESB
+    lda     #>(SCREEN+40*28)
+    sta     RESB+1
+
+    lda     #' '
+	BRK_TELEMON XFILLM
+.endproc	

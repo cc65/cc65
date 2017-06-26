bd883327ace3f7de952a48b394a8ce6b53636595
Greg King <gregdk@users.sf.net>
2017-06-26 02:34:23 -0400

    Added two functions that make it easier to write portable programs for Ohio Scientific computers.

---
+++ libsrc/osic1p/revers.s
@@ -0,0 +1,9 @@
+;
+; 2017-06-26, Greg King
+;
+; unsigned char __fastcall__ revers (unsigned char onoff)
+;
+
+        .import         return0
+
+        .export         _revers := return0      ; no attribute

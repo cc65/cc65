b6d3d32e88468eff8c10939efb8653833702512d
mrdudz <mrdudz@users.noreply.github.com>
2017-07-18 01:06:20 +0200

    wait for line 0 instead of last line

--- libsrc/vic20/waitvsync.s
+++ libsrc/vic20/waitvsync.s
@@ -8,20 +8,9 @@
 
         .include        "vic20.inc"
 
-; FIXME
-; this flag doesnt work on vic20!!!
-; it will have to be filled by a get_tv() constructor or so
-PALFLAG		= $2A6		; $01 = PAL, $00 = NTSC
-
 _waitvsync:
-        lda     PALFLAG
-        beq     @ntsc
-        ldx     #(312-8)/2
-        .byte $2c
-@ntsc:
-        ldx     #(262-8)/2
 @l2:
-        cpx     VIC_HLINE
-        bcs     @l2
+        lda     VIC_HLINE
+        bne     @l2
         rts
 

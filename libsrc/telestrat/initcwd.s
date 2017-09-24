fabb6acf3ffb53e4a20b4e163941c58d66dbdf39
jede <jede@oric.org>
2017-09-24 19:23:44 +0200

    Initcwd is now working for telestrat

---
+++ libsrc/telestrat/initcwd.s
@@ -0,0 +1,28 @@
+;
+; Oliver Schmidt, 18.04.2005
+;
+
+        .export         initcwd
+        .import         __cwd
+
+        .include        "zeropage.inc"
+        .include        "telestrat.inc"
+
+
+initcwd:
+        ldx     #PWD_PTR
+        BRK_TELEMON XVARS
+        sta     tmp1
+        sty     tmp1+1
+
+        ldy     #$00
+loop:        
+        lda     (tmp1),y
+        beq     done
+        sta     __cwd,y
+        iny
+        bne     loop
+       
+done:   
+        sta     (tmp2),y
+        rts

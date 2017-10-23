bd9b4ef60c9de6c4b2f0bad2a50d624daf066536
Oliver Schmidt <ol.sc@web.de>
2017-10-23 18:35:06 +0200

    Replaced three (logically) identical files with a single file.

--- libsrc/common/toascii.s
+++ libsrc/common/toascii.s
@@ -3,14 +3,8 @@
 ; /* Convert a target specific character to ascii */
 ;
 
-.export _toascii
+        .export         _toascii
 
-.proc _toascii
-
-; X must be zero on return
-        ldx     #0
-
-; Done!
+_toascii:
+        ldx     #>$0000
         rts
-
-.endproc

7d1c92dc129da98ec113817095f169d7266049ee
Stefan <stefan.haubenthal@gmail.com>
2017-11-21 10:33:48 +0100

    New API

--- libsrc/supervision/joy/supervision-stdjoy.s
+++ libsrc/supervision/joy/supervision-stdjoy.s
@@ -23,17 +23,6 @@
 
         .addr   $0000
 
-; Button state masks (8 values)
-
-        .byte   $08                     ; JOY_UP
-        .byte   $04                     ; JOY_DOWN
-        .byte   $02                     ; JOY_LEFT
-        .byte   $01                     ; JOY_RIGHT
-        .byte   $20                     ; JOY_FIRE_A
-        .byte   $10                     ; JOY_FIRE_B
-        .byte   $40                     ; JOY_SELECT
-        .byte   $80                     ; JOY_START
-
 ; Jump table.
 
         .addr   INSTALL

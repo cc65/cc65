a42b5c6dcef1a450ab04fdd74aa88f2afb8d1a3f
prandeamus <prandeamus@btinternet.com>
2017-12-13 22:32:12 +0000

    Tabs to spaces

--- samples/booth/booth.s
+++ samples/booth/booth.s
@@ -2,19 +2,18 @@
 ; Rob Burbidge, prandeamus@btinternet.com 
 ; December 2017
 ;
-		.include "zeropage.inc"
-		
-		.export  _booth8x8
-		.import  popa 	
+        .include "zeropage.inc"        
+        .export  _booth8x8
+        .import  popa   
 
 ; C Wrapper for signed integer 8x8
 ; int16_t __fastcall__ booth8x8(int8_t x, int8_t y);
 ; No major attempt has been made to optimise this
 ; except to optimise the jsr/rts at the end
-.proc	_booth8x8
-		tax 			; "y" parameter->X
-		jsr popa 		; "x" parameter->A
-        jmp booth8x8	; jsr booth8x8; rts
+.proc   _booth8x8
+        tax             ; "y" parameter->X
+        jsr popa        ; "x" parameter->A
+        jmp booth8x8    ; jsr booth8x8; rts
 .endproc
 
 ; 8-bit multiply A*X->AX using Booth's algorithm
@@ -23,76 +22,76 @@
 ; the A parameter. On exit, AX contains the result. Y and status are trashed.
 
 .proc   booth8x8
-		; ZP aliases for this routine
-		factorA = tmp1	; First factor - bits are rotated
-		factorX = ptr1	; Second factor - sign extended
-		product = ptr2 	; 16 bit product
-		
-		sta factorA		; Save factor A
-		
-		ldy #0			; Y=0
+        ; ZP aliases for this routine
+        factorA = tmp1  ; First factor - bits are rotated
+        factorX = ptr1  ; Second factor - sign extended
+        product = ptr2  ; 16 bit product
+        
+        sta factorA     ; Save factor A
+        
+        ldy #0          ; Y=0
 
-		; Zero product (16-bit)
-		sty product		; product = 0
-		sty product+1
-				
-		; Save X to factorX and sign-extend to factorX+1
-		stx factorX     ; low byte factorX
-		txa             ; 
-		ora #$7F        ; Now A is $FF or $7F
-		bmi skipSgn     ; Skip if negative ($FF)
-		tya             ; If zero/positive, set 0
+        ; Zero product (16-bit)
+        sty product     ; product = 0
+        sty product+1
+                
+        ; Save X to factorX and sign-extend to factorX+1
+        stx factorX     ; low byte factorX
+        txa             ; 
+        ora #$7F        ; Now A is $FF or $7F
+        bmi skipSgn     ; Skip if negative ($FF)
+        tya             ; If zero/positive, set 0
 skipSgn:sta factorX+1   ; Store high byte factorX
-		
-		iny				; Y is now 1
-		ldx #8			; Loop count: 8 bits
-		clc				; Additional bit in stream, initialised to 0
+        
+        iny             ; Y is now 1
+        ldx #8          ; Loop count: 8 bits
+        clc             ; Additional bit in stream, initialised to 0
 loopTop:
-		tya				; Y=1, equivalent to lda #1
-		and factorA		; Lowest bit of factorA. Sets S and Z, preserve C
+        tya             ; Y=1, equivalent to lda #1
+        and factorA     ; Lowest bit of factorA. Sets S and Z, preserve C
 
-		; Logic for 00, 01, 10, 11 
-		; Remember Z=1 means the low bit is zero)
-		bcs cSet
-		beq loopNxt		; C=0, Z=1
-		bne minus		; C=0, Z=0
-cSet:	bne loopNxt		; C=1, Z=0
-		;beq plus		; C=1, Z=1 (Fall through, not a branch)
-		
-plus:	; product += factorX (16 bit)
-		clc
-		lda product
-		adc factorX
-		sta product
-		lda product+1
-		adc factorX+1
-		sta product+1
-		
-		jmp loopNxt
-		
-minus:	; product -= factorX (16 bit)
-		sec
-		lda product
-		sbc factorX
-		sta product
-		lda product+1
-		sbc factorX+1
-		sta product+1
-		
-		; jmp loopNxt	; Fall through
+        ; Logic for 00, 01, 10, 11 
+        ; Remember Z=1 means the low bit is zero)
+        bcs cSet
+        beq loopNxt     ; C=0, Z=1
+        bne minus       ; C=0, Z=0
+cSet:   bne loopNxt     ; C=1, Z=0
+        ;beq plus       ; C=1, Z=1 (Fall through, not a branch)
+        
+plus:   ; product += factorX (16 bit)
+        clc
+        lda product
+        adc factorX
+        sta product
+        lda product+1
+        adc factorX+1
+        sta product+1
+        
+        jmp loopNxt
+        
+minus:  ; product -= factorX (16 bit)
+        sec
+        lda product
+        sbc factorX
+        sta product
+        lda product+1
+        sbc factorX+1
+        sta product+1
+        
+        ; jmp loopNxt   ; Fall through
 
 loopNxt:
-		; factorX *= 2 (16-bit)
-		asl factorX
-		rol factorX+1
-		; factorA shift right, low bit into C
-		lsr factorA 		; Bit 0->C
-		dex					; preserves C
-		bne loopTop
+        ; factorX *= 2 (16-bit)
+        asl factorX
+        rol factorX+1
+        ; factorA shift right, low bit into C
+        lsr factorA         ; Bit 0->C
+        dex                 ; preserves C
+        bne loopTop
 
-exit:	; product -> AX
-		lda product
-		ldx product+1
-		; Return to caller: AX is the product
-		rts
+exit:   ; product -> AX
+        lda product
+        ldx product+1
+        ; Return to caller: AX is the product
+        rts
 .endproc
--- samples/booth/boothtest.c
+++ samples/booth/boothtest.c
@@ -10,25 +10,25 @@
  * (for comparison with booth multiply)
  */
 int16_t __fastcall__ regular8x8(int8_t x, int8_t y) {
-	return x*y;
+    return x*y;
 }
 
 void do_regular8x8() {
-	int16_t x,y,z;
-	for(x=-127;x<=128;++x) {
-		for(y=-127;y<=128;++y) {
-			z=regular8x8((int8_t)x,(int8_t)y);
-		}
-	}	
+    int16_t x,y,z;
+    for(x=-127;x<=128;++x) {
+        for(y=-127;y<=128;++y) {
+            z=regular8x8((int8_t)x,(int8_t)y);
+        }
+    }   
 }
 
 void do_booth8x8() {
-	int16_t x,y,z;
-	for(x=-127;x<=128;++x) {
-		for(y=-127;y<=128;++y) {
-			z=booth8x8((int8_t)x,(int8_t)y);
-		}
-	}	
+    int16_t x,y,z;
+    for(x=-127;x<=128;++x) {
+        for(y=-127;y<=128;++y) {
+            z=booth8x8((int8_t)x,(int8_t)y);
+        }
+    }   
 }
 
 /* 
@@ -36,43 +36,43 @@ void do_booth8x8() {
  */
  
 void compare_booth_regular_8x8() {
-	int16_t x,y;
-	int16_t boothProduct;
-	int16_t product;
-	for(x=-127;x<=128;++x) {
-		for(y=-127;y<=128;++y) {			
-			boothProduct = booth8x8((int8_t)x, (int8_t)y);
-			product = regular8x8((int8_t)x, (int8_t)y);
-			assert(boothProduct==product);
-		}
-	}
+    int16_t x,y;
+    int16_t boothProduct;
+    int16_t product;
+    for(x=-127;x<=128;++x) {
+        for(y=-127;y<=128;++y) {            
+            boothProduct = booth8x8((int8_t)x, (int8_t)y);
+            product = regular8x8((int8_t)x, (int8_t)y);
+            assert(boothProduct==product);
+        }
+    }
 }
 
 void usage() {
-	puts("Usage: boothtest compare|regular|booth\n"); 
+    puts("Usage: boothtest compare|regular|booth\n"); 
 }
 
 /*
  * Simple test/benchmark framework
  */
 int main(int argc, char**argv) {
-	if (argc != 2) {
-		usage();
-		return 1;
-	}
-	else {
-		char *command = argv[1];
-		if(0==strcmp(command, "compare")) {
-			compare_booth_regular_8x8();
-		} else if(0==strcmp(command, "regular")) {
-			do_regular8x8();
-		} else if(0==strcmp(command, "booth")) {
-			do_booth8x8();
-		} else {
-			usage();
-			return 1;
-		}
-	}
-	return 0;
+    if (argc != 2) {
+        usage();
+        return 1;
+    }
+    else {
+        char *command = argv[1];
+        if(0==strcmp(command, "compare")) {
+            compare_booth_regular_8x8();
+        } else if(0==strcmp(command, "regular")) {
+            do_regular8x8();
+        } else if(0==strcmp(command, "booth")) {
+            do_booth8x8();
+        } else {
+            usage();
+            return 1;
+        }
+    }
+    return 0;
 }
 

2bfbd55e553d9538bea9994f39d060cb6c79f932
prandeamus <prandeamus@btinternet.com>
2017-12-15 00:59:04 +0000

    Refactor. Add 16x16 multiply. Bug fixes.

--- samples/booth/booth.s
+++ samples/booth/booth.s
@@ -8,33 +8,42 @@
 
 ; C wrapper for signed integer 8x8
 ; int16_t __fastcall__ booth8x8(int8_t x, int8_t y);
-; No major attempt has been made to optimise this
-; except to optimise the jsr/rts at the end
 .proc   _booth8x8
-        tax             ; "y" parameter->X
-        jsr popa        ; "x" parameter->A
-        jmp booth8x8    ; jsr booth8x8; rts
+
+        sta bFactor2    ; Second parameter in A
+        jsr popa        ; First parameter on C stack
+        sta bFactor1
+
+        jsr booth8x8
+
+        lda wProduct    ; Return value in AX
+        ldx wProduct+1  
+        
+        rts
 .endproc
 
 ; C wrapper for signed integer 16x16
 ; int32_t __fastcall__ booth16x16(int16_t, int16_t y);
 .proc   _booth16x16
         
-        sta wFactor1    ; Second parameter already loaded in AX
-        stx wFactor1+1
-        jsr popax       ; Load first parameter into AX
-        sta wFactor2
+        sta wFactor2    ; Second parameter in AX
         stx wFactor2+1
+        
+        jsr popax       ; First parameter on C stack
+        
+        sta wFactor1
+        stx wFactor1+1
         
         jsr booth16x16  
         
-        ; A/X/sreg = product
-        lda lProduct+3
-        sta sreg+1
-        lda lProduct+2
-        sta sreg
-        ldx lProduct+1
+        ; 32-bit product -> sreg/X/A
+        
         lda lProduct
+        ldx lProduct+1
+        ldy lProduct+2
+        sty sreg
+        ldy lProduct+3
+        sty sreg+1
         
         rts
 .endproc 
@@ -51,32 +60,30 @@
         wFactor2 = ptr2 ; Second factor - sign extended into ptr3
         lProduct = ptr4 ; 32-bit product tmp1,tmp2,tmp3,tmp4
         
-; 8-bit multiply A*X->AX using Booth's algorithm
+; 8-bit multiply bFactor1*bFactor2->wProduct using Booth's algorithm
 ; Compared to a regular multiplication, this will reduce the number of
 ; 16-bit additions when there is a run of consecutive 1's or 0's in
 ; the A parameter. On exit, AX contains the result. Y and status are trashed.
 
-.proc   booth8x8
+.proc   booth8x8       
         
-        sta bFactor1    ; Save A to bFactor1
-        
-        ldy #0          ; Y=0
+        ldy #0          ; Y=0 until further notice
 
         ; Zero product (16-bit)
         sty wProduct
         sty wProduct+1
                 
-        ; Save X to bFactor2 and sign-extend to bFactor2+1
-        stx bFactor2    ; low byte bFactor2
-        txa             ; 
+        ; Sign-extend bFactor2
+        lda bFactor2 
         ora #$7F        ; Now A is $FF or $7F
         bmi skipSgn     ; Skip if negative ($FF)
         tya             ; If zero/positive, set 0
 skipSgn:sta bFactor2+1  ; Store high byte bFactor2
         
-        iny             ; Y is now 1
-        ldx #8          ; Loop count: 8 bits
+        iny             ; Y is now 1 throughout the loop 
+        ldx #8          ; Bits to add
         clc             ; Additional bit in stream, initialised to 0
+
 loopTop:
         tya             ; Y=1, equivalent to lda #1
         and bFactor1    ; Lowest bit of bFactor1. Sets S and Z, preserve C
@@ -89,7 +96,7 @@ loopTop:
 cSet:   bne loopNxt     ; C=1, Z=0
         ;beq plus       ; C=1, Z=1 (Fall through, not a branch)
         
-plus:   ; product += bFactor2 (16 bit)
+plus:   ; wProduct += bFactor2 (16 bit)
         clc
         lda wProduct
         adc bFactor2
@@ -100,7 +107,7 @@ plus:   ; product += bFactor2 (16 bit)
         
         jmp loopNxt
         
-minus:  ; product -= bFactor2 (16 bit)
+minus:  ; wProduct -= bFactor2 (16 bit)
         sec
         lda wProduct
         sbc bFactor2
@@ -115,34 +122,101 @@ loopNxt:
         ; bFactor2 *= 2 (16-bit)
         asl bFactor2
         rol bFactor2+1
+        
         ; bFactor1 shift right, low bit into C
         lsr bFactor1        ; Bit 0->C
+        
+        ; Next loop
         dex                 ; preserves C
         bne loopTop
 
-exit:   ; product -> AX
-        lda wProduct
-        ldx wProduct+1
-        ; Return to caller: AX is the product
+exit:   
         rts
 .endproc
 
 ; lProduct = wFactor1*wFactor2 using Booth's algorithm
 .proc   booth16x16
 
-        ; DUMMY STUB initial version - just add the two numbers
+        ldy  #0     ; Y is 0 until further notice
+        
+        ; Zero product (32-bit)
+        
+        sty lProduct
+        sty lProduct+1
+        sty lProduct+2
+        sty lProduct+3 
+
+        ; Sign extend wFactor2
+        lda wFactor2+1  ; Upper byte of 2
+        ora #$7F        ; Now A is $FF or $7F
+        bmi skipSgn     ; Skip if negative ($FF)
+        tya             ; If zero/positive, set 0
+skipSgn:sta wFactor2+2  ; Store high byte wFactor2+2
+        sta wFactor2+3  ; Store high byte wFactor2+3
+
+        iny             ; Y is now 1 throughout the loop 
+        ldx #16         ; Bits to add
+        clc             ; Additional bit in stream, initialised to 0
+loopTop:
+        tya             ; Y=1, equivalent to lda #1
+        and wFactor1    ; Lowest bit of wFactor1. Sets S and Z, preserve C
+
+        ; Logic for 00, 01, 10, 11 
+        ; Remember Z=1 means the low bit is zero)
+        bcs cSet
+        beq loopNxt     ; C=0, Z=1
+        bne minus       ; C=0, Z=0
+cSet:   bne loopNxt     ; C=1, Z=0
+        ;beq plus       ; C=1, Z=1 (Fall through, not a branch)
+        
+plus:   ; lProduct += wFactor2 (32 bit)
         clc
-        lda wFactor1
+        lda lProduct
         adc wFactor2
         sta lProduct
-        lda wFactor1+1
+        lda lProduct+1
         adc wFactor2+1
         sta lProduct+1
-        lda #0
-        adc #0
+        lda lProduct+2
+        adc wFactor2+2
         sta lProduct+2
-        lda #0
+        lda lProduct+3
+        adc wFactor2+3
+        sta lProduct+3
+
+        jmp loopNxt
+        
+minus:  ; lProduct -= wFactor2 (32 bit)
+        sec
+        lda lProduct
+        sbc wFactor2
+        sta lProduct
+        lda lProduct+1
+        sbc wFactor2+1
+        sta lProduct+1
+        lda lProduct+2
+        sbc wFactor2+2
+        sta lProduct+2
+        lda lProduct+3
+        sbc wFactor2+3
         sta lProduct+3
         
+        ; jmp loopNxt   ; Fall through
+
+loopNxt:
+        ; wFactor2 *= 2 (32-bit)
+        asl wFactor2
+        rol wFactor2+1
+        rol wFactor2+2
+        rol wFactor2+3        
+        
+        ; wFactor1 shift right, low bit into C
+        lsr wFactor1+1
+        ror wFactor1        ; Bit 0->C
+        
+        ; Next loop
+        dex                 ; preserves C
+        bne loopTop
+
         rts
 .endproc
--- samples/booth/boothtest.c
+++ samples/booth/boothtest.c
@@ -6,41 +6,68 @@
 #include "booth.h"
 
 /*
- * Call regular multiply with a function call overhead
+ * Call regular 8x8 multiply with a function call overhead
  * (for comparison with booth multiply)
  */
 int16_t __fastcall__ regular8x8(int8_t x, int8_t y) {
-    return x*y;
+    return ((int16_t)x)*y; /* One argument must be promoted for wide result */
+}
+
+/*
+ * Call regular 16x16 multiply with a function call overhead
+ * (for comparison with booth multiply)
+ */
+
+int32_t __fastcall__ regular16x16(int16_t x, int16_t y) {
+    return ((int32_t)x)*y; /* One argument must be promoted for wide result */
 }
 
 void do_regular8x8() {
     int16_t x,y,z;
-    for(x=-127;x<=128;++x) {
-        for(y=-127;y<=128;++y) {
+    for(x=INT8_MIN;x<=INT8_MAX;++x) {
+        for(y=INT8_MIN;y<=INT8_MAX;++y) {
             z=regular8x8((int8_t)x,(int8_t)y);
+        }
+    }   
+}
+
+void do_regular16x16() {
+    int32_t x,y,z;
+    for(x=-500;x<=500;++x) {
+        for(y=-500;y<=500;++y) {
+            z=regular16x16((int16_t)x,(int16_t)y);
         }
     }   
 }
 
 void do_booth8x8() {
     int16_t x,y,z;
-    for(x=-127;x<=128;++x) {
-        for(y=-127;y<=128;++y) {
+    for(x=INT8_MIN;x<=INT8_MAX;++x) {
+        for(y=INT8_MIN;y<=INT8_MAX;++y) {
             z=booth8x8((int8_t)x,(int8_t)y);
         }
     }   
 }
 
+
+void do_booth16x16() {
+    int32_t x,y,z;
+    for(x=-500;x<=500;++x) {
+        for(y=-500;y<=500;++y) {
+            z=booth16x16((int16_t)x,(int16_t)y);
+        }
+    }   
+}
+
+
 /* 
- * Regression test: compare booth to internal multiply routines 
+ * Regression test: compare booth 8x8 to internal multiply routines 
  */
  
 void compare_booth_regular_8x8() {
-    int16_t x,y;
-    int16_t boothProduct;
-    int16_t product;
-    for(x=-127;x<=128;++x) {
-        for(y=-127;y<=128;++y) {            
+    int16_t x,y, boothProduct, product;
+    for(x=INT8_MIN;x<=INT8_MAX;++x) {
+        for(y=INT8_MIN;y<=INT8_MAX;++y) {            
             boothProduct = booth8x8((int8_t)x, (int8_t)y);
             product = regular8x8((int8_t)x, (int8_t)y);
             assert(boothProduct==product);
@@ -48,8 +75,27 @@ void compare_booth_regular_8x8() {
     }
 }
 
+/* 
+ * Regression test: compare booth 16x16 to internal multiply routines 
+ */
+ 
+void compare_booth_regular_16x16() {
+    int32_t x,y, boothProduct, product;
+    for(x=-500;x<=500;++x) {
+        for(y=-500;y<=500;++y) {            
+            boothProduct = booth16x16((int16_t)x, (int16_t)y);
+            product = regular16x16((int16_t)x, (int16_t)y);
+            /*assert(boothProduct==product);*/
+            if(boothProduct != product) {
+                printf("%d x %d expected %08lx, got %08lx\n", (int16_t)x, (int16_t)y, (int32_t)product, (int32_t)boothProduct);
+                assert(0);
+            }
+        }
+    }
+}
+
 void usage() {
-    puts("Usage: boothtest compare|regular|booth\n"); 
+    puts("Usage: boothtest compare[8|16]|regular[8|16]|booth[8|16]\n"); 
 }
 
 /*
@@ -62,12 +108,18 @@ int main(int argc, char**argv) {
     }
     else {
         char *command = argv[1];
-        if(0==strcmp(command, "compare")) {
+        if       (0==strcmp(command, "compare8")) {
             compare_booth_regular_8x8();
-        } else if(0==strcmp(command, "regular")) {
+        } else if(0==strcmp(command, "compare16")) {
+            compare_booth_regular_16x16();  
+        } else if(0==strcmp(command, "regular8")) {
             do_regular8x8();
-        } else if(0==strcmp(command, "booth")) {
+        } else if(0==strcmp(command, "regular16")) {
+            do_regular16x16();
+        } else if(0==strcmp(command, "booth8")) {
             do_booth8x8();
+        } else if(0==strcmp(command, "booth16")) {
+            do_booth16x16();
         } else {
             usage();
             return 1;

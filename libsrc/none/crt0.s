45482b4fb1fdf69d149cbdd5aa7aafdbc816020d
bauen1 <j2468h@gmail.com>
2018-01-04 22:14:01 +0100

    Fixed none/crt0.s to respect none.cfg

--- libsrc/none/crt0.s
+++ libsrc/none/crt0.s
@@ -2,15 +2,14 @@
        .export         __STARTUP__ : absolute = 1      ; Mark as startup
        .import         zerobss, _main
        .import         initlib, donelib
-       .import         __MAIN_START__, __MAIN_SIZE__   ; Linker generated
-       .import         __STACKSIZE__                   ; Linker generated
+       .import         __STACKSTART__, __STACKSIZE__   ; Linker generated
 
        .include "zeropage.inc"
 
        .segment "STARTUP"
 
-       lda #<(__MAIN_START__ + __MAIN_SIZE__ + __STACKSIZE__)
-       ldx #<(__MAIN_START__ + __MAIN_SIZE__ + __STACKSIZE__)
+       lda #<__STACKSTART__
+       lda #>__STACKSTART__
        sta sp
        stx sp+1
        jsr zerobss

99c54a74d07742fa6ec420a7ad525df177d5279a
prandeamus <prandeamus@btinternet.com>
2018-01-06 23:59:38 +0000

    Use cc65_imul8x8r16 and cc65_imul16x16r32 for more meaningful comparison

--- samples/booth/boothtest.c
+++ samples/booth/boothtest.c
@@ -2,31 +2,15 @@
 #include <stdint.h>
 #include <assert.h>
 #include <string.h>
+#include <cc65.h>
 
 #include "booth.h"
-
-/*
- * Call regular 8x8 multiply with a function call overhead
- * (for comparison with booth multiply)
- */
-int16_t __fastcall__ regular8x8(int8_t x, int8_t y) {
-    return ((int16_t)x)*y; /* One argument must be promoted for wide result */
-}
-
-/*
- * Call regular 16x16 multiply with a function call overhead
- * (for comparison with booth multiply)
- */
-
-int32_t __fastcall__ regular16x16(int16_t x, int16_t y) {
-    return ((int32_t)x)*y; /* One argument must be promoted for wide result */
-}
 
 void do_regular8x8() {
     int16_t x,y,z;
     for(x=INT8_MIN;x<=INT8_MAX;++x) {
         for(y=INT8_MIN;y<=INT8_MAX;++y) {
-            z=regular8x8((int8_t)x,(int8_t)y);
+            z=cc65_imul8x8r16((int8_t)x,(int8_t)y);
         }
     }   
 }
@@ -36,7 +20,7 @@ void do_regular16x16() {
     int32_t z;
     for(x=-500;x<=500;++x) {
         for(y=-500;y<=500;++y) {
-            z=regular16x16((int16_t)x,(int16_t)y);
+            z=cc65_imul16x16r32((int16_t)x,(int16_t)y);
         }
     }   
 }
@@ -69,7 +53,7 @@ void compare_booth_regular_8x8() {
     for(x=INT8_MIN;x<=INT8_MAX;++x) {
         for(y=INT8_MIN;y<=INT8_MAX;++y) {            
             boothProduct = booth8x8((int8_t)x, (int8_t)y);
-            product = regular8x8((int8_t)x, (int8_t)y);
+            product = cc65_imul8x8r16((int8_t)x, (int8_t)y);
             assert(boothProduct==product);
         }
     }
@@ -85,7 +69,7 @@ void compare_booth_regular_16x16() {
     for(x=-500;x<=500;++x) {
         for(y=-500;y<=500;++y) {            
             boothProduct = booth16x16((int16_t)x, (int16_t)y);
-            product = regular16x16((int16_t)x, (int16_t)y);
+            product = cc65_imul16x16r32((int16_t)x, (int16_t)y);
             assert(boothProduct==product);
         }
     }

5d4116f05fa311fb4611a932218cb9ef073d147a
Marco van den Heuvel <blackystardust68@yahoo.com>
2018-03-20 13:11:24 -0700

    Added isfast function which returns a 1 when the C128 is in 2MHz mode.

---
+++ libsrc/c128/isfast.s
@@ -0,0 +1,20 @@
+;
+; Marco van den Heuvel, 2018-03-19
+;
+; unsigned char isfast (void);
+; /* Returns 1 if the CPU is in 2MHz mode. */
+;
+
+        .export         _isfast
+
+        .include        "c128.inc"
+
+
+.proc   _isfast
+
+        lda     VIC_CLK_128
+        and     #$01
+        rts
+
+.endproc
+

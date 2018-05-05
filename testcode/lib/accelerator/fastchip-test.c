cecc7fef1bddcbd779d9b087f56ad5277797fa94
Marco van den Heuvel <blackystardust68@yahoo.com>
2018-05-04 22:14:02 -0700

    Added apple-IIe fast chip accelerator code and documentation.

---
+++ testcode/lib/accelerator/fastchip-test.c
@@ -0,0 +1,8 @@
+/* Apple IIe Fast Chip accelerator test code. */
+
+#define ACC_DETECT      detect_fastchip
+#define ACC_GET_SPEED   get_fastchip_speed
+#define ACC_SET_SPEED   set_fastchip_speed
+#define ACC_NAME        "Apple IIe Fast Chip slot card"
+
+#include "turbo-test.c"

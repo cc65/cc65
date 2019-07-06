3d5811d8f5ef8b14e39635c7ec9fb9f01ca6c62f
jede <jede@oric.org>
2019-07-06 10:19:45 +0200

    Fix gotoy changecolor

--- libsrc/telestrat/gotoy.s
+++ libsrc/telestrat/gotoy.s
@@ -10,5 +10,11 @@
 .proc _gotoy
    sta     SCRY
    jsr     _update_adscr
+
+   ; We change the current line, it means that we need to put color attributes again.
+   ; That is not the case with _gotox because, it's on the same line attribute are already set
+   lda     #$01             
+   sta     CHARCOLOR_CHANGE
+   sta     BGCOLOR_CHANGE
    rts
 .endproc

ac4866c027b881b9cde2b4195cac0988eb3ca411
Greg King <gregdk@users.sf.net>
2019-11-10 12:46:01 -0500

    Made assert() send SIGABRT when an assertion fails.
    A signal handler can catch it, and do anything needed to make stderr work.

--- libsrc/common/_afailed.c
+++ libsrc/common/_afailed.c
@@ -2,11 +2,12 @@
 ** _afailed.c
 **
 ** 1998-06-06, Ullrich von Bassewitz
-** 2015-03-13, Greg King
+** 2019-11-10, Greg King
 */
 
 
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,9 +15,7 @@
 
 void __fastcall__ _afailed (char* file, unsigned line)
 {
+    raise (SIGABRT);
     fprintf (stderr, "ASSERTION FAILED IN %s(%u)\n", file, line);
     exit (2);
 }
-
-
-

fb0d09a277ce74959552fe1ce58f1b4d7ee8419f
Greg King <gregdk@users.sf.net>
2019-12-06 14:47:47 -0500

    Changed sim65's internal error codes from 9-bit values to 7-bit values.
    Some shells truncate process return codes to 8 bits.  And, the eigth bit often is used to show that a signal stopped the process.

--- src/sim65/error.h
+++ src/sim65/error.h
@@ -49,10 +49,10 @@
 
 
 
-#define SIM65_ERROR         256
+#define SIM65_ERROR         0x7F
 /* Does not use EXIT_FAILURE because it may overlap with test results. */
 
-#define SIM65_ERROR_TIMEOUT 257
+#define SIM65_ERROR_TIMEOUT 0x7E
 /* An error result for max CPU instructions exceeded. */
 
 

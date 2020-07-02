8449c9eaa03563d64894f462ff604fc9efd498a7
Jesse Rosenstock <jmr@users.noreply.github.com>
2020-07-02 09:08:57 +0200

    Add zlib license text

--- test/val/bitfield.c
+++ test/val/bitfield.c
@@ -1,13 +1,25 @@
 /*
-  !!DESCRIPTION!! Tests of bit-field packing
-  !!ORIGIN!!      cc65 regression tests
-  !!LICENCE!!     zlib
-  !!AUTHOR!!      Jesse Rosenstock
-  !!COPYRIGHT!!   Copyright 2020 Google LLC
+  Copyright 2020 Google LLC
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 */
 
 /*
-  see https://github.com/cc65/cc65/issues/1054
+  Tests of bit-field packing; see https://github.com/cc65/cc65/issues/1054
 */
 
 #include <stdio.h>

cc65/cc65

commit 6dc2bf12265f106d8cd4916a73990873f62f5c36

Rename PaddingBitWidth to PaddingBits

src/cc65/declare.c
@@ -773,7 +773,7 @@ static SymEntry* ParseStructDecl (const char* Name)
             if (BitOffs > 0) {
                 if (FieldWidth <= 0 || (BitOffs + FieldWidth) > INT_BITS) {
                     /* Bits needed to byte-align the next field. */
-                    unsigned PaddingBitWidth = -BitOffs % CHAR_BITS;
+                    unsigned PaddingBits = -BitOffs % CHAR_BITS;
 
                     /* We need an anonymous name */
                     AnonName (Ident, "bit-field");
@@ -781,10 +781,10 @@ static SymEntry* ParseStructDecl (const char* Name)
                     /* Add an anonymous bit-field that aligns to the next
                     ** byte.
                     */
-                    AddBitField (Ident, StructSize, BitOffs, PaddingBitWidth);
+                    AddBitField (Ident, StructSize, BitOffs, PaddingBits);
 
                     /* No bits left */
-                    StructSize += (BitOffs + PaddingBitWidth) / CHAR_BITS;
+                    StructSize += (BitOffs + PaddingBits) / CHAR_BITS;
                     BitOffs = 0;
                 }
             }

commit 9d19aba7a1a0ed89db7168e0ceaf86ba18f7c5b4

Fix MSVC build broken by #1058
MSVC complains about unary negation of unsigned, but it's
intended.  Suppress the warning.

https://github.com/cc65/cc65/pull/1058#discussion_r451757967

"Tested" with godbolt.org.

src/cc65/declare.c
@@ -772,7 +772,11 @@ static SymEntry* ParseStructDecl (const char* Name)
             */
             if (BitOffs > 0) {
                 if (FieldWidth <= 0 || (BitOffs + FieldWidth) > INT_BITS) {
-                    /* Bits needed to byte-align the next field. */
+                    /* Bits needed to byte-align the next field.  MSVC complains
+                    ** about unary negation of unsigned, but it's intended.
+                    ** Disable the warning for the next line only.
+                    */
+                    #pragma warning(disable: 4146)
                     unsigned PaddingBits = -BitOffs % CHAR_BITS;
 
                     /* We need an anonymous name */
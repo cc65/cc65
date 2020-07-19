ba1c979b3fc447a6a597b261bf575cb55453d4ff
acqn <acqn163@outlook.com>
2020-07-19 21:23:20 +0800

    Disabled applying 'sizeof' to bit-fields.

--- src/cc65/expr.c
+++ src/cc65/expr.c
@@ -1898,14 +1898,19 @@ void hie10 (ExprDesc* Expr)
                 CodeMark Mark;
                 GetCodePos (&Mark);
                 hie10 (Expr);
-                /* If the expression is a literal string, release it, so it
-                ** won't be output as data if not used elsewhere.
-                */
-                if (ED_IsLocLiteral (Expr)) {
-                    ReleaseLiteral (Expr->LVal);
+                if (ED_IsBitField (Expr)) {
+                    Error ("Cannot apply 'sizeof' to bit-field");
+                    Size = 0;
+                } else {
+                    /* If the expression is a literal string, release it, so it
+                    ** won't be output as data if not used elsewhere.
+                    */
+                    if (ED_IsLocLiteral (Expr)) {
+                        ReleaseLiteral (Expr->LVal);
+                    }
+                    /* Calculate the size */
+                    Size = CheckedSizeOf (Expr->Type);
                 }
-                /* Calculate the size */
-                Size = CheckedSizeOf (Expr->Type);
                 /* Remove any generated code */
                 RemoveCode (&Mark);
             }

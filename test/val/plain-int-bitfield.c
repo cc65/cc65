ab89c168decdb959251c28314bd29f20943fba33
mrdudz <mrdudz@users.noreply.github.com>
2020-08-18 17:47:35 +0200

    replace "Copyright 2020 Google LLC" by "Copyright 2020 the cc65 authors"

--- test/val/plain-int-bitfield.c
+++ test/val/plain-int-bitfield.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2020 Google LLC
+  Copyright 2020 the cc65 authors
 
   This software is provided 'as-is', without any express or implied
   warranty. In no event will the authors be held liable for any damages

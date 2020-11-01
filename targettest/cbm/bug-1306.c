d5f762f71e0bfd4f686640dcc5ca2cf5ad7da734
Sven Michael Klose <pixel@hugbox.org>
2020-11-01 22:17:30 +0100

    Bug #1306: check if cbm_read() works at end of file.

---
+++ targettest/cbm/bug-1306.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cbm.h>
+
+void
+main ()
+{
+    char * data = malloc (1024);
+    unsigned len;
+
+    cbm_open (8, 8, 8, "read.c");
+
+    //cbm_read() always returns at end of file.
+    while ((len = cbm_read (8, data, 1024)) > 0)
+        printf ("%d ", len);
+
+    /* NOEXIT */
+}

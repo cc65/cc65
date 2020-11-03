cfa8bc3036029e631e81a690dfebe7d9524c2ee7
Sven Michael Klose <pixel@hugbox.org>
2020-11-03 11:50:10 +0100

    Try to keep track of EOFs to return 0 after the last byte.

--- libsrc/cbm/cbm_close.s
+++ libsrc/cbm/cbm_close.s
@@ -5,6 +5,22 @@
 ;
 
         .import _cbm_k_close
+        .import cbm_read_ended_files
         .export _cbm_close
 
-_cbm_close = _cbm_k_close
+MAX_LFNS        =   10          ; Maximum number of logical file numbers open at once.
+
+.proc _cbm_close
+; Remove LFN from list of ended files.
+        lda     $B8             ; LFN
+        ldx     #MAX_LFNS - 1
+@L1:    cmp     cbm_read_ended_files,x
+        beq     @L2
+        dex
+        bpl     @L1
+        bmi     @L3             ; (jmp)
+
+@L2:    lda     #0
+        sta     cbm_read_ended_files,x
+@L3:    jmp _cbm_k_close
+.endproc
--- libsrc/cbm/cbm_read.s
+++ libsrc/cbm/cbm_read.s
@@ -4,45 +4,22 @@
 ; Original C code by Marc 'BlackJack' Rintsch, 19.03.2001
 ;
 ; int __fastcall__ cbm_read (unsigned char lfn, void* buffer, unsigned int size)
-; /* Reads up to "size" bytes from a file to "buffer".
-; ** Returns the number of actually read bytes, 0 if there are no bytes left
-; ** (EOF) or -1 in case of an error. _oserror contains an errorcode then (see
-; ** table below).
-; */
-; {
-;     static unsigned int bytesread;
-;     static unsigned char tmp;
-;
-;     /* if we can't change to the inputchannel #lfn then return an error */
-;     if (_oserror = cbm_k_chkin(lfn)) return -1;
-;
-;     bytesread = 0;
-;
-;     while (bytesread<size && !cbm_k_readst()) {
-;         tmp = cbm_k_basin();
-;
-;         /* the kernal routine BASIN sets ST to EOF if the end of file
-;         ** is reached the first time, then we have store tmp.
-;         ** every subsequent call returns EOF and READ ERROR in ST, then
-;         ** we have to exit the loop here immediatly.
-;         */
-;         if (cbm_k_readst() & 0xBF) break;
-;
-;         ((unsigned char*)buffer)[bytesread++] = tmp;
-;     }
-;
-;     cbm_k_clrch();
-;     return bytesread;
-; }
-;
 
         .include        "cbm.inc"
 
         .export         _cbm_read
+        .export         cbm_read_ended_files = ended_files
         .importzp       ptr1, ptr2, ptr3, tmp1
         .import         popax, popa
         .import         __oserror
 
+MAX_LFNS        =   10          ; Maximum number of logical file numbers open at once.
+
+        .data
+
+ended_files:    .res MAX_LFNS
+
+        .code
 
 _cbm_read:
         eor     #$FF
@@ -55,30 +32,29 @@ _cbm_read:
         sta     ptr2
         stx     ptr2+1          ; Save buffer
 
-        jsr     popa
-        tax
-        jsr     CHKIN
-        bcs     @E1             ; Branch on error
-
-; bytesread = 0;
-
         lda     #$00
         sta     ptr3
-        sta     ptr3+1
-        beq     @L3             ; Branch always
+        sta     ptr3+1          ; bytesread = 0;
+
+        jsr     popa
+        pha
+        tax
+        jsr     CHKIN
+        pla
+        bcs     @E1             ; Branch on error
+
+; Check if file has been read completely already.
+        ldx     #MAX_LFNS - 1
+@L5:    cmp ended_files,x
+        beq     @L4
+        dex
+        bpl     @L5
+
+        bmi     @L3             ; (jmp)
 
 ; Loop
 
 @L1:    jsr     BASIN           ; Read next char from file
-        sta     tmp1            ; Save it for later
-
-        jsr     READST
-        cmp     #$40
-        beq     @L4             ; Done
-        cmp     #0              ; Status ok?
-        bne     @E1
-
-        lda     tmp1
         ldy     #0
         sta     (ptr2),y        ; Save read byte
 
@@ -89,6 +65,12 @@ _cbm_read:
 @L2:    inc     ptr3
         bne     @L3
         inc     ptr3+1          ; ++bytesread;
+
+        jsr     READST
+        cmp     #$40
+        beq     @L6             ; Done
+        cmp     #0              ; Status ok?
+        bne     @E1
 
 @L3:    inc     ptr1
         bne     @L1
@@ -102,9 +84,20 @@ _cbm_read:
 
         rts
 
+; EOF reached, add LFN to list.
+@L6:    ldx     #MAX_LFNS - 1
+@L7:    lda     ended_files,x
+        beq     @L8
+        dex
+        bpl     @L7
+
 ; CHKIN failed
 
 @E1:    sta     __oserror
         lda     #$FF
         tax
         rts                     ; return -1
+
+@L8:    lda     $B8             ; LFN
+        sta     ended_files,x
+        bne     @L4             ; (jmp)

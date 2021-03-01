256b22f1c7017f5f30c3e6046cf4e641825788ef
jede <jede@oric.org>
2021-03-01 22:36:09 +0100

    Add bordercolor and cvline

---
+++ libsrc/telestrat/bordercolor.s
@@ -0,0 +1,15 @@
+;
+; Ullrich von Bassewitz, 06.08.1998
+;
+; unsigned char __fastcall__ bordercolor (unsigned char color);
+;
+
+
+        .export         _bordercolor
+
+        .include        "telestrat.inc"
+
+_bordercolor:
+        ; Nothing to do
+        ; Oric can't handle his border
+        rts

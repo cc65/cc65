2f9115cfd65c55874f3b2180458baf504cb0ef90
jede <jede@oric.org>
2021-03-03 22:20:20 +0100

    Fix tab and jmp

--- libsrc/telestrat/syschdir.s
+++ libsrc/telestrat/syschdir.s
@@ -6,7 +6,7 @@
 
         .export         __syschdir
         .import         addysp, popax
-	    .importzp       tmp1
+        .importzp       tmp1
         .import         initcwd
 
         .include        "telestrat.inc"
@@ -29,6 +29,4 @@ __syschdir:
         
         BRK_TELEMON(XPUTCWD)
 
-        jsr     initcwd      ; Update cwd
-        
-        rts
+        jmp     initcwd      ; Update cwd

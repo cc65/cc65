bbb1af7b357ea5fcc862095dbb7c08c1f289ff3e
jede <jede@oric.org>
2021-03-15 23:28:26 +0100

    Clean close

--- libsrc/telestrat/close.s
+++ libsrc/telestrat/close.s
@@ -2,15 +2,11 @@
 
     .export         _close
 
-    .import         addysp,popax
-
     .include        "zeropage.inc"
     .include        "telestrat.inc"
-    .include        "errno.inc"
-    .include        "fcntl.inc"
 
 ; int open (const char* name, int flags, ...);    /* May take a mode argument */
 .proc _close
-    BRK_TELEMON XCLOSE  ; launch primitive ROM
+    BRK_TELEMON XCLOSE  ; Launch primitive ROM
     rts
 .endproc

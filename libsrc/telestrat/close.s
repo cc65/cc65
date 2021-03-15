665cb84909ac9af38a11b72cd59cd334ff8acfe1
jede <jede@oric.org>
2021-03-15 23:30:37 +0100

    Remove wrong proto

--- libsrc/telestrat/close.s
+++ libsrc/telestrat/close.s
@@ -2,10 +2,8 @@
 
     .export         _close
 
-    .include        "zeropage.inc"
     .include        "telestrat.inc"
 
-; int open (const char* name, int flags, ...);    /* May take a mode argument */
 .proc _close
     BRK_TELEMON XCLOSE  ; Launch primitive ROM
     rts

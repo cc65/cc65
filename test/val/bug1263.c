5d05451ab2e5e4b9e52c99a5d6d04dfb5ac7ca7e
acqn <acqn163@outlook.com>
2021-04-01 11:41:55 +0800

    Fixed test case for Issue #1263.

--- test/val/bug1263.c
+++ test/val/bug1263.c
@@ -1,9 +1,16 @@
 /* bug #1263 - erroneous error for K & R function declaration */
 
 enum E { I = 0 };
-extern int f(enum E);
 
+extern int f();
 int f(e)
+  enum E e;
+{
+  return e;
+}
+
+extern int g(int);
+int g(e)
   enum E e;
 {
   return e;
@@ -11,5 +18,5 @@ int f(e)
 
 int main(void)
 {
-    return f(I);
+    return f(I) + g(I);
 }

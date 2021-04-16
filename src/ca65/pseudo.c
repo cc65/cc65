4ceb006888a75fde1ea35e9c4eb28650f771ca38
Marco Aurelio da Costa <costa@gamic.com>
2021-04-16 14:42:16 -0300

    Implemented charmap stack
    New commands:
     .PUSHCHARMAP: will push the current charmap state into an internal stack
     .POPCHARMAP: will restore the current charmap to the last pushed charmap
    
    Details:
      The push and pop facilities are implemented directly inside the tgttrans.h,
      to facilitate its reuse on the C compiler.

--- src/ca65/pseudo.c
+++ src/ca65/pseudo.c
@@ -1568,6 +1568,19 @@ static void DoPageLength (void)
 
 
 
+static void DoPopCharmap (void)
+/* Restore a charmap */
+{
+    if (TgtTranslateStackIsEmpty ()) {
+        ErrorSkip ("Charmap stack is empty");
+        return;
+    }
+
+    TgtTranslatePop ();
+}
+
+
+
 static void DoPopCPU (void)
 /* Pop an old CPU setting from the CPU stack */
 {
@@ -1653,6 +1666,16 @@ static void DoPSC02 (void)
 /* Switch to 65SC02 CPU */
 {
     SetCPU (CPU_65SC02);
+}
+
+
+
+static void DoPushCharmap (void)
+/* Save the current charmap */
+{
+    if (!TgtTranslatePush ()) {
+        ErrorSkip ("Charmap stack overflow");
+    }
 }
 
 
@@ -2101,10 +2124,12 @@ static CtrlDesc CtrlCmdTab [] = {
     { ccNone,           DoUnexpected    },      /* .PARAMCOUNT */
     { ccNone,           DoPC02          },
     { ccNone,           DoPDTV          },
+    { ccNone,           DoPopCharmap    },
     { ccNone,           DoPopCPU        },
     { ccNone,           DoPopSeg        },
     { ccNone,           DoProc          },
     { ccNone,           DoPSC02         },
+    { ccNone,           DoPushCharmap   },
     { ccNone,           DoPushCPU       },
     { ccNone,           DoPushSeg       },
     { ccNone,           DoUnexpected    },      /* .REFERENCED */
@@ -2182,5 +2207,8 @@ void CheckPseudo (void)
     }
     if (!IS_IsEmpty (&CPUStack)) {
         Warning (1, "CPU stack is not empty");
+    }
+    if (!TgtTranslateStackIsEmpty ()) {
+        Warning (1, "Charmap stack is not empty");
     }
 }

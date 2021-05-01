8d7cb137cacf7e6f69ebc8c87c589bc1ca3369bb
jede <jede@oric.org>
2021-05-01 19:04:35 +0200

    Now kbhit stores the value in order to send it to cgetc in the next cgetc call

--- libsrc/telestrat/kbhit.s
+++ libsrc/telestrat/kbhit.s
@@ -6,14 +6,20 @@
 
         .export         _kbhit
 
+        .export         store_char
+
         .include        "telestrat.inc"
 
 _kbhit:
         BRK_TELEMON XRD0
         ldx     #$00 
         bcs     @no_char_action
+        sta     store_char
         lda     #$01
         rts
 @no_char_action:
         tax        
         rts
+.data        
+store_char:
+        .byte 0

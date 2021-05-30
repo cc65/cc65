af3d4581d306f00c4a7f0aba62f9d79962a3ff79
Greg King <gregdk@users.sf.net>
2021-05-30 13:30:08 -0400

    Moved Creativision's playsound() into a separate file.
    It won't waste space in a cartridge if it isn't used.

--- libsrc/creativision/psg.s
+++ libsrc/creativision/psg.s
@@ -1,27 +1,18 @@
 ; void __fastcall__ psg_outb (unsigned char b);
 ; void __fastcall__ psg_delay (unsigned char b);
-; void __fastcall__ bios_playsound (void *a, unsigned char b);
 ; void psg_silence (void);
 
 
         .export         _psg_outb, _psg_silence, _psg_delay
-        .export         _bios_playsound
-
-        .import         popax
 
         .include        "creativision.inc"
 
 
-songptr :=      $00             ; Points to current tune data
-volptr  :=      $04             ; Points to current volume table
-
-_psg_outb:
-        ;* Let BIOS output the value.
-        jmp     $FE77
+;* Let BIOS output the value.
+_psg_outb       := BIOS_POKE_PSG
 
 
-_psg_silence:
-        jmp     $FE54
+_psg_silence    := BIOS_QUIET_PSG
 
 
 _psg_delay:
@@ -37,31 +28,3 @@ l3:     sbc     #1
         dey
         bne     l1
         rts
-
-
-;* Creativision Sound Player
-;* Based on BIOS song player.
-;*
-;* Pass a pointer to a set of note triples, terminated with a tempo byte;
-;* and pass the length of the triples and tempo (max 255).
-;*
-;* Note: tune data must be stored backwards.
-
-_bios_playsound:
-        php
-        pha                     ; Save tune length
-        sei
-
-        lda     #<$FCD5         ; A BIOS volume table
-        ldx     #>$FCD5
-        sta     volptr
-        stx     volptr+1
-
-        jsr     popax           ; Get tune array pointer
-        sta     songptr
-        stx     songptr+1
-
-        pla
-        tay
-        dey                     ; Point to tempo byte
-        jmp     $FBED           ; Let BIOS do its thing

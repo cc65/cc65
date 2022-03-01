3266e70de4431a8994d922d119009b56a0b73690
Karri Kaksonen <karri@sipo.fi>
2022-03-01 06:44:55 +0200

    Add target atari7800

--- src/cc65/main.c
+++ src/cc65/main.c
@@ -172,6 +172,10 @@ static void SetSys (const char* Sys)
             DefineNumericMacro ("__ATARI5200__", 1);
             break;
 
+        case TGT_ATARI7800:
+            DefineNumericMacro ("__ATARI7800__", 1);
+            break;
+
         case TGT_ATARI:
             DefineNumericMacro ("__ATARI__", 1);
             break;

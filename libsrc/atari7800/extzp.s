d5dcea65b77c5f3598bf07d86a420afe431a5d4c
Karri Kaksonen <karri@sipo.fi>
2022-04-05 11:13:18 +0300

    Allocate own zp pointer for conio implementation

---
+++ libsrc/atari7800/extzp.s
@@ -0,0 +1,11 @@
+;
+; Karri Kaksonen, 2022-04-05
+;
+; zeropage locations for exclusive use by the library
+;
+
+        .include "extzp.inc"
+
+        .segment "EXTZP" : zeropage
+
+ptr7800:        .res 2

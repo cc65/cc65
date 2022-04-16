4b492ed4e56a449d3439ea892631b121bbeb66bd
mrdudz <mrdudz@users.noreply.github.com>
2022-04-16 18:18:51 +0200

    added TAB checker script, to be added to the PR checker action

---
+++ .github/checks/tabs.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+OLDCWD=`pwd`
+SCRIPT_PATH=`dirname $0`
+cd $SCRIPT_PATH/../../
+
+FILES=`find $CHECK_PATH -type f \( \( -name \*.inc -a \! -name Makefile.inc \) -o -name \*.cfg -o -name \*.c -o -name \*.s -o -name \*.h -o -name \*.asm -o -name \*.sgml \) -print | xargs grep -l $'\t' | grep -v "libwrk/" | grep -v "testwrk/"`
+
+cd $OLDCWD
+
+if [ x"$FILES"x != xx ]; then
+    echo "error: found TABs in the following files:"
+    for n in $FILES; do
+        echo $n
+    done
+    exit -1
+fi

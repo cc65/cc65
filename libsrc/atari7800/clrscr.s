422d245cab2dc16f10b58a5cbc66b60b80237644
Karri Kaksonen <karri@sipo.fi>
2022-04-16 21:13:51 +0300

    Split files

--- libsrc/atari7800/clrscr.s
+++ libsrc/atari7800/clrscr.s
@@ -3,34 +3,34 @@
 
         .export _clrscr
 
-	.import	_screen
-	.import	pusha0,pushax
-	.include "extzp.inc"
+        .import _screen
+        .import pusha0,pushax
+        .include "extzp.inc"
 
-	.code
+        .code
 
-	.proc _clrscr
+        .proc _clrscr
 
-	lda	#<(_screen)
-	ldx	#>(_screen)
-	sta	ptr7800
-	stx	ptr7800+1
-	ldx	#screenrows
-@L1:	ldy	#charsperline
-	lda	#0
-@L2:	sta	(ptr7800),y
-	dey
-	bne	@L2
-	lda	ptr7800
-	clc
-	adc	#charsperline
-	bcc	@L3
-	inc	ptr7800+1
-@L3:	dex
-	bne	@L1
-	rts
+        lda     #<(_screen)
+        ldx     #>(_screen)
+        sta     ptr7800
+        stx     ptr7800+1
+        ldx     #screenrows
+@L1:    ldy     #charsperline
+        lda     #0
+@L2:    sta     (ptr7800),y
+        dey
+        bne     @L2
+        lda     ptr7800
+        clc
+        adc     #charsperline
+        bcc     @L3
+        inc     ptr7800+1
+@L3:    dex
+        bne     @L1
+        rts
 
-	.endproc
+        .endproc
 
 ;-------------------------------------------------------------------------------
 ; force the init constructor to be imported
---
+++ libsrc/atari7800/textcolor.s
@@ -0,0 +1,58 @@
+;
+; Karri Kaksonen, 2022-04-16
+;
+;
+
+        .export         _textcolor
+        .export         txtcolor
+
+        .include        "atari7800.inc"
+
+        .data
+;-----------------------------------------------------------------------------
+; Holder of the text colour offset
+; 0 = red, 42 = green, 84 = white
+;
+txtcolor:
+        .byte   0
+
+        .code
+
+;-----------------------------------------------------------------------------
+; Change the text colour
+;
+; Logical colour names are
+; 0 = red
+; 1 = green
+; 2 = white
+;
+; The routine will also return the previous textcolor
+;
+        .proc   _textcolor
+
+        beq     @L2
+        sec
+        sbc     #1
+        beq     @L1
+        lda     #84
+        jmp     @L2
+@L1:    lda     #42
+@L2:    ldy     txtcolor
+        sta     txtcolor        ; Store new textcolor
+        tya
+        bne     @L3
+        rts                     ; Old colour was 0
+@L3:    sec
+        sbc     #42
+        bne     @L4
+        lda     #1
+        rts                     ; Old colour was 1
+@L4:    lda #2
+        rts                     ; Old colour was 2
+        .endproc
+
+;-------------------------------------------------------------------------------
+; force the init constructor to be imported
+
+                .import initconio
+conio_init      = initconio

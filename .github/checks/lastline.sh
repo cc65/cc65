2cdccd5e8a9784ef642014818ae99fa50254e723
Greg King <gregdk@users.sf.net>
2022-04-17 19:06:35 -0400

    Oops, forgot to check macros.

--- .github/checks/lastline.sh
+++ .github/checks/lastline.sh
@@ -9,7 +9,7 @@ nl='
 '
 nl=$'\n'
 r1="${nl}$"
-FILES=`find $CHECK_PATH -type f \( -name \*.inc -o -name Makefile -o -name \*.cfg -o -name \*.\[chs\] -o -name \*.asm -o -name \*.sgml \) -print | while read f; do
+FILES=`find $CHECK_PATH -type f \( -name \*.inc -o -name Makefile -o -name \*.cfg -o -name \*.\[chs\] -o -name \*.mac -o -name \*.asm -o -name \*.sgml \) -print | while read f; do
     t=$(tail -c2 $f; printf x)
     [[ ${t%x} =~ $r1 ]] || echo "$f"
 done`

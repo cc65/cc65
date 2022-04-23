244da9c91150ad67e2dc2df35264d149a36b8ca8
jede <jede@oric.org>
2022-04-24 00:39:04 +0200

    missing fd_management and cleaning

--- libsrc/telestrat/read.s
+++ libsrc/telestrat/read.s
@@ -2,38 +2,41 @@
 ; jede jede@oric.org 2017-01-22
 ;
 
-    .export         _read
+        .export         _read
 
-    .import         popax
+        .import         popax
 
-    .include        "zeropage.inc"
-    .include        "telestrat.inc"
+        .include        "zeropage.inc"
+        .include        "telestrat.inc"
 
 ;int read (int fd, void* buf, unsigned count);
 
 .proc   _read
 
-    sta     ptr1           ; Count
-    stx     ptr1+1         ; Count
-    jsr     popax          ; Get buf
+        sta     ptr1           ; count
+        stx     ptr1+1         ; count
+        jsr     popax          ; get buf
 
-    sta     PTR_READ_DEST
-    stx     PTR_READ_DEST+1
-    sta     ptr2           ; In order to calculate nb of bytes read
-    stx     ptr2+1 ;
+        sta     PTR_READ_DEST
+        stx     PTR_READ_DEST+1
+        sta     ptr2           ; in order to calculate nb of bytes read
+        stx     ptr2+1 
 
+        jsr     popax          ; get FD
 
-    lda     ptr1 ;
-    ldy     ptr1+1 ;
-    BRK_TELEMON     XFREAD ; calls telemon30 routine
-    ;  Compute nb of bytes read
-    lda     PTR_READ_DEST+1
-    sec
-    sbc     ptr2+1
-    tax
-    lda     PTR_READ_DEST
-    sec
-    sbc     ptr2
-    ; Here A and X contains number of bytes read
-    rts
+        tax                    ; send FD to X
+
+        lda     ptr1
+        ldy     ptr1+1 
+        BRK_TELEMON     XFREAD 
+        ;  compute nb of bytes read
+        lda     PTR_READ_DEST+1
+        sec
+        sbc     ptr2+1
+        tax
+        lda     PTR_READ_DEST
+        sec
+        sbc     ptr2
+        ; here A and X contains number of bytes read
+        rts
 .endproc

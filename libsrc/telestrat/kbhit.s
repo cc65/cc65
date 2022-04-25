e9597e872101604e382468235cdba69a1ab8bf6b
jede <jede@oric.org>
2022-04-26 00:40:16 +0200

    cgetc export data now

--- libsrc/telestrat/kbhit.s
+++ libsrc/telestrat/kbhit.s
@@ -6,12 +6,24 @@
 
         .export         _kbhit
 
+        .import         store_char
+
         .include        "telestrat.inc"
 
 _kbhit:
-        BRK_TELEMON XRD0
+        lda     store_char       ; Check if a key has been detected previously   
+        beq     @call_telemon    ; No, calls Telemon routine
+        lda     #$01             ; There is a key pressed previously, return 1
         ldx     #$00
-        txa
-        rol
-        eor     #$01
+        rts
+@call_telemon:        
+        BRK_TELEMON XRD0
+
+        ldx     #$00
+        bcs     @no_char_action
+        sta     store_char
+        lda     #$01
+        rts
+@no_char_action:
+        tax
         rts

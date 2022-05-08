cc65/cc65

commit 676b14429dcb1357602639ea7ed455fcc48c73df

Better opt chance for certain optimization steps e.g. OptPtrStore1 etc.

src/cc65/codeopt.c
@@ -1241,10 +1241,10 @@ static unsigned RunOptGroup1 (CodeSeg* S)
 
     Changes += RunOptFunc (S, &DOptGotoSPAdj, 1);
     Changes += RunOptFunc (S, &DOptStackPtrOps, 5);
+    Changes += RunOptFunc (S, &DOptAdd3, 1);    /* Before OptPtrLoad5! */
     Changes += RunOptFunc (S, &DOptPtrStore1, 1);
     Changes += RunOptFunc (S, &DOptPtrStore2, 1);
     Changes += RunOptFunc (S, &DOptPtrStore3, 1);
-    Changes += RunOptFunc (S, &DOptAdd3, 1);    /* Before OptPtrLoad5! */
     Changes += RunOptFunc (S, &DOptPtrLoad1, 1);
     Changes += RunOptFunc (S, &DOptPtrLoad2, 1);
     Changes += RunOptFunc (S, &DOptPtrLoad3, 1);
@@ -1332,7 +1332,6 @@ static unsigned RunOptGroup3 (CodeSeg* S)
         C += RunOptFunc (S, &DOptAdd6, 1);
         C += RunOptFunc (S, &DOptJumpCascades, 1);
         C += RunOptFunc (S, &DOptDeadJumps, 1);
-        C += RunOptFunc (S, &DOptRTS, 1);
         C += RunOptFunc (S, &DOptDeadCode, 1);
         C += RunOptFunc (S, &DOptBoolTrans, 1);
         C += RunOptFunc (S, &DOptJumpTarget1, 1);
@@ -1487,11 +1486,16 @@ static unsigned RunOptGroup7 (CodeSeg* S)
     /* Adjust branch distances */
     Changes += RunOptFunc (S, &DOptBranchDist, 3);
 
-    /* Replace conditional branches to RTS. If we had changes, we must run dead
-    ** code elimination again, since the change may have introduced dead code.
-    */
+    /* Replace conditional branches to RTS */
     C = RunOptFunc (S, &DOptRTSJumps2, 1);
+
+    /* Replace JSR followed by RTS to JMP */
+    C += RunOptFunc (S, &DOptRTS, 1);
+
     Changes += C;
+    /* If we had changes, we must run dead code elimination again,
+    ** since the changes may have introduced dead code.
+    */
     if (C) {
         Changes += RunOptFunc (S, &DOptDeadCode, 1);
     }

commit e6b9a4b2924f6e2bbb5f6e1c62e8c2e5cc7423e8

test related to issue #1652, shows regression broken by #1231 and the case that #1231 improved.

test/val/bug1652-optimizer.c
@@ -1,0 +1,122 @@
+
+/* #1652 - Regression in code optimization */
+
+#include <stdlib.h>
+
+/* better before pr#1231
+
+before:
+
+        jsr     pusha
+        ldy     #$01
+        ldx     #$00
+        lda     (sp),y
+        beq     L0005
+        lda     (sp,x)
+        beq     L0005
+        txa
+        jmp     incsp2          ; <--
+L0005:  tya
+        jmp     incsp2
+
+after:
+
+        jsr     pusha
+        ldy     #$01
+        ldx     #$00
+        lda     (sp),y
+        beq     L0004
+        lda     (sp,x)
+        beq     L0004
+        txa
+        jmp     L0001            ; <--
+L0004:  tya
+L0001:  jmp     incsp2
+
+*/
+
+int foo(unsigned char bar, unsigned char baz)
+{
+    return (bar == 0 || baz == 0);
+}
+
+/* worse before pr#1231
+
+before:
+;
+; y = 1;
+;
+    lda     #$01
+    sta     _y
+;
+; (*tablePtr)[y][x] = 83;
+;
+    ldx     #$00
+    lda     _y
+    jsr     aslax2
+    clc
+    adc     _tablePtr
+    pha
+    txa
+    adc     _tablePtr+1
+    tax
+    pla
+    clc
+    adc     _x
+    bcc     L0019
+    inx
+L0019:
+    sta     ptr1
+    stx     ptr1+1
+    lda     #$53
+    ldy     #$00
+    sta     (ptr1),y
+
+after:
+
+;
+; y = 1;
+;
+    lda     #$01
+    sta     _y
+;
+; (*tablePtr)[y][x] = 83;
+;
+    ldx     #$00
+    lda     _y
+    jsr     aslax2
+    clc
+    adc     _tablePtr
+    sta     ptr1
+    txa
+    adc     _tablePtr+1
+    sta     ptr1+1
+    ldy     _x
+    lda     #$53
+    sta     (ptr1),y
+*/
+
+typedef unsigned char array_t[4][4];
+
+static array_t table = {
+    {12, 13, 14, 15},
+    { 8,  9, 10, 11},
+    { 4,  5,  6,  7},
+    { 0,  1,  2,  3}
+};
+static array_t *tablePtr = &table;
+
+static unsigned char y = 0, x;
+
+void pointed_array(void)
+{
+    x = 3;
+    y = 1;
+    (*tablePtr)[y][x] = 83;
+}
+
+int main(void)
+{
+    pointed_array();
+    return foo(2, 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
48ae6716addecfcc0deccd6f5bb5c1411d81f2d5
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2022-06-16 20:57:53 +0200

    Some test cases for ca65

---
+++ test/asm/listing/020-asciiz.s
@@ -0,0 +1,14 @@
+; 2022-06-15 Spiro Trikaliotis
+
+
+	; upper case pseudo-op
+	.ASCIIZ ""
+	.ASCIIZ "Hello World"
+	.ASCIIZ "Hello ","World"
+	.ASCIIZ "Hello ","World"," ","I"," ","am"," ","testing"
+
+	; lower case pseudo-op
+	.asciiz ""
+	.asciiz "Hello World"
+	.asciiz "Hello ","World"
+	.asciiz "Hello ","World"," ","I"," ","am"," ","testing"

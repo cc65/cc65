7476c86ba9dbda053ca863cd90364fadf87973c4
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2022-06-20 16:33:25 +0200

    More .assert testcases
    Add testcases with error generation.

--- test/asm/listing/032-assert-error3.s
+++ test/asm/listing/032-assert-error3.s
@@ -1,8 +1,3 @@
-; 2022-06-18 Spiro Trikaliotis
+; 2022-06-20 Spiro Trikaliotis
 
         .assert * = $0000, error, "Code not at $0000"
-        .assert * = $0001, error, "Code not at $0001"
-        .assert * = $1000, error, "Code not at $1000"
-        .assert * = $1001, error, "Code not at $1001"
-        .assert * = $8000, error, "Code not at $8000"
-        .assert * = $8001, error, "Code not at $8001"

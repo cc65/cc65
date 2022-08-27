41b29d5c81f26879b79d2093295e2764387951d5
mrdudz <mrdudz@users.noreply.github.com>
2022-08-27 02:10:02 +0200

    add testcase related to issue #1252

---
+++ test/misc/bug1252.c
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+
+int main(void)
+{
+// this works
+c_label:
+  asm("inx\n"
+      "bne %g\n",
+      c_label); 
+
+// this does not work
+c_label2:
+  asm("inx\n"
+      "bne %g        \n",
+      c_label2); 
+
+    return EXIT_SUCCESS;
+}

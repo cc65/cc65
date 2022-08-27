168f42bb836ba940ec7aedaf76356d3facf7d595
mrdudz <mrdudz@users.noreply.github.com>
2022-08-27 02:14:52 +0200

    fix whitespace

--- test/misc/bug1252.c
+++ test/misc/bug1252.c
@@ -7,13 +7,13 @@ int main(void)
 c_label:
   asm("inx\n"
       "bne %g\n",
-      c_label); 
+      c_label);
 
 // this does not work
 c_label2:
   asm("inx\n"
       "bne %g        \n",
-      c_label2); 
+      c_label2);
 
     return EXIT_SUCCESS;
 }

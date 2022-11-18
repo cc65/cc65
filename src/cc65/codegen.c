a6feeac5ed1115ffcf5910215699907a24bd8980
mrdudz <mrdudz@users.noreply.github.com>
2022-11-18 18:10:07 +0100

    or msvc I64u ?

--- src/cc65/codegen.c
+++ src/cc65/codegen.c
@@ -69,7 +69,7 @@
    mingw and PRIXPTR */
 #if defined(__MINGW64__)
 #undef PRIXPTR
-#define PRIXPTR "zu"
+#define PRIXPTR "%I64u"
 #endif
 
 /*****************************************************************************/

4c6fddd3000efb4aa1d105232cdeebd1b122b4ca
mrdudz <mrdudz@users.noreply.github.com>
2022-11-18 17:58:12 +0100

    i want a cookie each time i do this

--- src/cc65/codegen.c
+++ src/cc65/codegen.c
@@ -69,7 +69,7 @@
    mingw and PRIXPTR */
 #if defined(__MINGW64__)
 #undef PRIXPTR
-#define PRIXPTR "l"
+#define PRIXPTR "lu"
 #endif
 
 /*****************************************************************************/

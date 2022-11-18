b166dc4261fee8dd7dab7be743dcaca6948831f4
mrdudz <mrdudz@users.noreply.github.com>
2022-11-18 18:19:31 +0100

    hopefully thats it

--- src/cc65/codegen.c
+++ src/cc65/codegen.c
@@ -65,8 +65,12 @@
 #include "util.h"
 #include "codegen.h"
 
-/* this is a terrible hack that tries to combat the ever reoccuring issue with
-   mingw and PRIXPTR */
+/* This is a terrible hack that tries to combat the ever reoccuring issue with
+   Mingw and PRIXPTR - the macro should have been defined like this for us in
+   the first place.
+   NOTE: "I64u" works in the github actions now, so if your local mingw64 fails,
+         you probably have to update.
+*/
 #if defined(__MINGW64__)
 #undef PRIXPTR
 #define PRIXPTR "I64u"

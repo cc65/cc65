ca576adfe504c91dbd29b5653ad3433200d505ea
Bob Andrews <mrdudz@users.noreply.github.com>
2022-12-11 12:44:44 +0100

    add assert that checks int promotion

--- test/val/bug1941.c
+++ test/val/bug1941.c
@@ -6,8 +6,9 @@
 uint8_t foo=1U;
 uint8_t bar=3U; // You need it to reproduce the issue
 
+_Static_assert (sizeof (foo >> 0) == sizeof (int), "Result type of (foo >> 0) should be int");
+
 int main(void)
 {
     return(foo == foo << 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
-

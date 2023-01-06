2a961a870b2486bf1a4edd2eb4e00faf57fe1064
Mariano Dominguez <mariano.dm@gmail.com>
2023-01-06 09:56:15 -0800

    add target test

---
+++ targettest/atari/sound.c
@@ -0,0 +1,20 @@
+/*
+** testprogram printing the default device
+**
+**  January 6 2023 Mariano Domínguez
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <atari.h>
+#include <cc65.h>
+
+int main(void)
+{
+    int i=0;
+    printf("playing sound \n");
+    sound(1,121,10,15);
+    for(i=0;i<90;i++);
+    sound(1,0,0,0);
+    return 0;
+}

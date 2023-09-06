dfe7562f76185b141795f1db1bad905a5f4b1acc
Colin Leroy-Mira <colin@colino.net>
2023-09-06 08:09:00 +0200

    Add ntohs, htons, ntohl, htons.

---
+++ include/arpa/inet.h
@@ -0,0 +1,10 @@
+#ifndef _ARPA_INET_H
+#define _ARPA_INET_H
+
+int __fastcall__ ntohs (int val);
+int __fastcall__ htons (int val);
+
+long __fastcall__ ntohl (long val);
+long __fastcall__ htonl (long val);
+
+#endif

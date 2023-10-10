6cd039469cb315e9755290085f87c175b7fbdcfb
Dave Plummer <davepl@davepl.com>
2023-10-09 17:22:09 -0700

    DrawCircle in ASM

--- samples/kim1/subs.asm
+++ samples/kim1/subs.asm
@@ -15,41 +15,49 @@
 .export _ClearScreen
 .export _ScrollScreen
 .export _SetPixel
+.export _ClearPixel
 .export _DrawCircle
 
 SCREEN  = $A000
 
 .segment "ZEROPAGE"
-temp:        .res 1
 btpt:        .res 1
+
 _x1cord:      .res 2
 _x2cord:      .res 2
 _y1cord:      .res 2
 _y2cord:      .res 2
+
+dest:
+dest_lo:      .res 1
+dest_hi:      .res 1
+
+src:
+src_lo:       .res 1
+src_hi:       .res 1
+
+adp1:
+adp1_lo:      .res 1
+adp1_hi:      .res 1
+
+adp2:
+adp2_lo:      .res 1
+adp2_hi:      .res 1
 
 .exportzp _x1cord
 .exportzp _x2cord
 .exportzp _y1cord
 .exportzp _y2cord
 
+.segment "DATA"
 
-dest    = $02
-dest_lo = dest
-dest_hi = dest+1
-
-src    = $04
-src_lo = src
-src_hi = src+1
-
-adp1    = $06
-adp1_lo = adp1
-adp1_hi = adp1+1
-
-adp2    = $08
-adp2_lo = adp2
-adp2_hi = adp2+1
-
-
+xval:           .res 2
+yval:           .res 2 
+err:            .res 2  
+temp:           .res 2
+temp2:          .res 2
+x0:             .res 2
+y0:             .res 2
 
 .segment "CODE"
 
@@ -64,7 +72,7 @@ adp2_hi = adp2+1
 ;-----------------------------------------------------------------------------------
 
 _GetPixelAddress:          
-                lda 	_x1cord 		; compute bit address first
+                lda 	_x1cord 	; compute bit address first
                 sta 	adp1 		; also transfer _x1cord to adp1
                 and 	#$07		; + which is simply the low 3 bits of x
                 sta 	btpt
@@ -133,7 +141,7 @@ msktb2:	  	    .byte 	$7F,$BF,$DF,$EF,$F7,$FB,$FD,$FE
 ; Mask tables for individual pixel subroutines
 ;-----------------------------------------------------------------------------------
 
-_SetPixel:	jsr 	_GetPixelAddress
+_SetPixel:	    jsr 	_GetPixelAddress
                 ldy 	btpt 		; get bit number in y
                 lda 	msktb1,y 	; get a byte with that bit =1, others =0
                 ldy 	#0 		
@@ -226,22 +234,43 @@ _ScrollScreen:
 ; y             - _y1cord (16-bit)   
 ; radius        - _y2cord (16-bit)
 ;-----------------------------------------------------------------------------------
+; Implements the midpoint circle algorithm without floating point or trig functions
+;-----------------------------------------------------------------------------------
 
-xval:           .res 2
-yval:           .res 2 
-err:            .res 2  
-tempsum:        .res 2
-tempx:          .res 2
-tempy:          .res 2
-        
-_DrawCircle:    lda _x1cord                      ; tempx = _x1cord
-                sta tempx
+; void DrawCircle(int x0, int y0, int radius, byte val) 
+; {
+;     int x = radius;
+;     int y = 0;
+;     int err = 0;
+; 
+;     while (x >= y) 
+;     {
+;         SETPIXEL(x0 + x, y0 + y, val);
+;         SETPIXEL(x0 + y, y0 + x, val);
+;         SETPIXEL(x0 - y, y0 + x, val);
+;         SETPIXEL(x0 - x, y0 + y, val);
+;         SETPIXEL(x0 - x, y0 - y, val);
+;         SETPIXEL(x0 - y, y0 - x, val);
+;         SETPIXEL(x0 + y, y0 - x, val);
+;         SETPIXEL(x0 + x, y0 - y, val);
+; 
+;         y++;
+;         err += 1 + 2 * y;
+;         if (2 * (err - x) + 1 > 0) {
+;             x--;
+;             err += 1 - 2 * x;
+;         }
+;     }
+; }
+       
+_DrawCircle:    lda _x1cord                      ; x0 = _x1cord
+                sta x0
                 lda _x1cord+1
-                sta tempx+1
-                lda _y1cord                      ; tempy = _y1cord
-                sta tempy
+                sta x0+1
+                lda _y1cord                      ; y0 = _y1cord
+                sta y0
                 lda _y1cord+1
-                sta tempy+1
+                sta y0+1
 
                 lda _y2cord                      ; x = radius
                 sta xval
@@ -254,218 +283,221 @@ _DrawCircle:    lda _x1cord                      ; tempx = _x1cord
                 sta err                         ; err = 0;
                 sta err+1                       
 circleloop:                        
-                lda xval+1                      ; if (xval < yval) goto done;
+                lda xval+1                      ; if (xval < yval) we're done;
+                sec
                 cmp yval+1
-                bcs docircle
-                lda xval
-                cmp yval
-                bcs docircle
-                rts
-docircle:
-                lda tempx
+                bcc doneCircle                  ; if high byteof yval is greater, we can draw
+                bne doCircle                    ; it not greater and not equal, is less, so done
+                lda xval                        ; in other cases we need to compare the LSB next
+                cmp yval                            
+                bcs doCircle                    ; if it's less, but MSB was equal, we go draw
+
+doneCircle:     rts
+
+doCircle:                                       
+                lda x0                          ; Draw the first of 8 symmetric quadrant copies
                 clc
                 adc yval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 adc yval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 sec
                 sbc xval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 sbc xval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 + y, y0 - x, val);
-                
-                lda tempx
+   
+   
+                lda x0
                 sec
                 sbc yval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 sbc yval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 sec
                 sbc xval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 sbc xval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 - y, y0 - x, val);
-                
-                lda tempx
+             
+                lda x0
                 sec
                 sbc xval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 sbc xval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 sec
                 sbc yval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 sbc yval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 - x, y0 - y, val);
                 
-                lda tempx
+                lda x0
                 sec
                 sbc xval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 sbc xval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 clc
                 adc yval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 adc yval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 - x, y0 + y, val);
                 
-                lda tempx
+                lda x0
                 clc
                 adc yval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 adc yval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 clc
                 adc xval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 adc xval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 + y, y0 + x, val);
                 
-                lda tempx
+                lda x0
                 clc
                 adc xval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 adc xval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 clc
                 adc yval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 adc yval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 + x, y0 + y, val);
                 
-                lda tempx
+                lda x0
                 clc
                 adc xval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 adc xval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 sec
                 sbc yval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 sbc yval+1
                 sta _y1cord+1
                 jsr _SetPixel                    ; SETPIXEL(x0 + x, y0 - y, val);
                 
-                lda tempx
+                lda x0
                 sec
                 sbc yval
                 sta _x1cord
-                lda tempx+1
+                lda x0+1
                 sbc yval+1
                 sta _x1cord+1
-                lda tempy
+                lda y0
                 clc
                 adc xval
                 sta _y1cord
-                lda tempy+1
+                lda y0+1
                 adc xval+1
                 sta _y1cord+1
-                jsr _SetPixel                    ; SETPIXEL(x0 - y, y0 + x, val);
-                
+                jsr _SetPixel                   ; SETPIXEL(x0 - y, y0 + x, val);
+     
                 inc yval                        ; yval++;
-                bcc :+
+                bne :+
                 inc yval+1
-:               
-                lda #0                         ; tempsum = 2 * yval + 1;
-                sta tempsum
-                sta tempsum+1
-                lda yval
+
+:               lda yval                        ; temp = 2 * yval + 1;
                 asl
-                rol yval+1
+                sta temp
+                lda yval+1
+                rol 
+                sta temp+1
+                inc temp
+                bne :+
+                inc temp+1
+:               
+                lda err                         ; err += temp
                 clc
-                adc #01
-                sta tempsum
-                bcc :+
-                inc tempsum+1
-                
-                lda err                         ; err += tempsum
-                clc
-                adc tempsum
+                adc temp
                 sta err
-                bcc :+
                 lda err+1
-                adc tempsum+1
+                adc temp+1
                 sta err+1
-:                
-                lda err                         ; tempsum = err-xval                              
+                                                ; if (2 * (err - xval) + 1 > 0) then dec xval
+                lda err                         ; temp = err-xval                              
                 sec
                 sbc xval
-                sta tempsum
-                bcs :+
+                sta temp
                 lda err+1
                 sbc xval+1
-                sta tempsum+1
+                sta temp+1
                 
-                asl tempsum                     ; tempsum = 2*(err-xval)
-                rol tempsum+1
-                                
-                clc                             ; tempsum = 2*(err-xval)+1
-                inc tempsum
-                bcc :+
-                inc tempsum+1
-:
-                lda tempsum+1                    ; if (tempsum <= 0) don't inc the xval
-                beq noadd
-                bmi noadd
-                
-                sec                             ; xval--
-                dec xval
-                bcs noadd
-                dec xval+1
- noadd:               
-                lda xval                        ; tempsum = xval * 2
+                lda temp                     ; temp will now be 2*(err-xval)+1
                 asl
-                sta tempsum
+                sta temp
+                lda temp+1
+                rol
+                sta temp+1
+                inc temp                     
+                bne :+
+                inc temp+1
+:               
+                lda temp+1                   ; if (temp > 0) we'll dec xval
+                bmi doneLoop                   ; less than zero, so no dec
+                bne decxval                     ; if not zero, go ahead and dec
+
+                lda temp                     ; MSb is zero so now check the LSB
+                beq doneLoop                   ; both bytes are zero, so no dec
+
+decxval:        lda xval                        ; xval--
+                bne :+
+                dec xval+1
+:               dec xval
+
+updateerr:      lda xval                        ; temp = xval * 2
+                asl
+                sta temp
                 lda xval+1
                 rol
-                sta tempsum+1
-                
+                sta temp+1
 
-                lda #1                          ; tempsum = 1-(xval*2)
+                lda #1                          ; temp2 = 1-(xval*2)
                 sec
-                sbc tempsum
-                sta tempsum
+                sbc temp
+                sta temp2
                 lda #0
-                sbc tempsum+1
-                sta tempsum+1
+                sbc temp+1
+                sta temp2+1
                                 
                 lda err                         ; err += 1-(xval*2)
                 clc
-                adc tempsum
+                adc temp2
                 sta err
                 lda err+1
-                adc tempsum+1
+                adc temp2+1
                 sta err+1
                 
-                jmp circleloop
-donecircle:       
-                rts
+doneLoop:       jmp circleloop
                 

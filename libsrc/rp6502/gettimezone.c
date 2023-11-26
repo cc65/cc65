3905e430680869169cd820589b95bb6d9974176e
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-11-26 15:12:20 -0800

    style update

--- libsrc/rp6502/gettimezone.c
+++ libsrc/rp6502/gettimezone.c
@@ -8,8 +8,9 @@ int clock_gettimezone (clockid_t clock_id, struct _timezone* tz)
     ax = ria_call_int_errno (RIA_OP_CLOCK_GETTIMEZONE);
     if (ax >= 0) {
         char i;
-        for (i = 0; i < sizeof (struct _timezone); i++)
+        for (i = 0; i < sizeof (struct _timezone); i++) {
             ((char*)tz)[i] = ria_pop_char ();
+        }
     }
     return ax;
 }
--- libsrc/rp6502/randomize.c
+++ libsrc/rp6502/randomize.c
@@ -1,7 +1,6 @@
 #include <rp6502.h>
 #include <stdlib.h>
 
-// Non-standard cc65
 void _randomize (void)
 {
     srand (ria_call_int (RIA_OP_LRAND));

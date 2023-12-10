befc9533c6195e9cc18aa4873b823d486dba0abd
acqn <acqn163@outlook.com>
2023-12-10 20:21:50 +0800

    More accurate diagnostic messages on empty declarations without any type specifiers.

--- src/cc65/expr.c
+++ src/cc65/expr.c
@@ -1412,7 +1412,7 @@ static void Primary (ExprDesc* E)
             } else {
                 /* Let's see if this is a C99-style declaration */
                 DeclSpec Spec;
-                ParseDeclSpec (&Spec, TS_DEFAULT_TYPE_NONE, SC_AUTO);
+                ParseDeclSpec (&Spec, TS_DEFAULT_TYPE_INT, SC_AUTO);
 
                 if ((Spec.Flags & DS_TYPE_MASK) != DS_NONE) {
                     Error ("Mixed declarations and code are not supported in cc65");

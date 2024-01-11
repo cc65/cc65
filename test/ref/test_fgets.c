c1993d25d91133ce68ce0a352ecca099170654af
Colin Leroy-Mira <colin@colino.net>
2024-01-11 18:30:13 +0100

    add fgets test

---
+++ test/ref/test_fgets.c
@@ -0,0 +1,57 @@
+/*
+  !!DESCRIPTION!! fgets test
+  !!LICENCE!!     Public domain
+*/
+
+#include "common.h"
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *in, *out;
+char buf[32];
+
+#define INFILE   "cf.in"
+#define OUTFILE  "test_fgets.out"
+
+#ifdef NO_OLD_FUNC_DECL
+int main(int argc,char **argv)
+#else
+main(argc, argv)
+int argc;
+char *argv[];
+#endif
+{
+    out = fopen(OUTFILE, "wb");
+    if (out == NULL) {
+        return EXIT_FAILURE;
+    }
+    if (fgets(buf, sizeof(buf), out) != NULL) {
+      printf("Error, could fgets with write-only file\n");
+      return 1;
+    }
+    if (!ferror(out)) {
+      printf("Error: file pointer should be in error state\n");
+    }
+    fclose(out);
+
+    in = fopen(INFILE, "rb");
+    if (in == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    if (fgets(NULL, 0, in) != NULL) {
+      printf("Error, could fgets with zero size\n");
+      return 1;
+    }
+
+    while (fgets(buf, sizeof(buf), in) != NULL)
+    {
+        printf("%s",buf);
+    }
+
+    fclose(in);
+    return 0;
+}

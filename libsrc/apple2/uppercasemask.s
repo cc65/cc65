66ee8eca9d6e44c8e8889c49573a327e8c3ce765
Colin Leroy-Mira <colin@colino.net>
2025-05-21 20:47:21 +0200

    Apple2: automatically enable lowercase starting from //e

--- libsrc/apple2/uppercasemask.s
+++ libsrc/apple2/uppercasemask.s
@@ -4,6 +4,25 @@
 
         .export         uppercasemask
 
+        .ifndef __APPLE2ENH__
+        .import         machinetype
+        .constructor    detectlowercase
+        .endif
+
+        .ifndef __APPLE2ENH__
+
+        .segment "ONCE"
+
+detectlowercase:
+        bit     machinetype
+        bpl     :+
+
+        lda     #$FF
+        sta     uppercasemask
+:       rts
+
+        .endif
+
         .data
 
 uppercasemask:  .byte   $DF     ; Convert to uppercase

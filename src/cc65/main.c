3c6e6819ce4b17b278132510f6185f0a9f827699
Bob Andrews <mrdudz@users.noreply.github.com>
2025-06-12 22:34:50 +0200

    Update main.c - spaces

--- src/cc65/main.c
+++ src/cc65/main.c
@@ -306,7 +306,7 @@ static void SetSys (const char* Sys)
         case TGT_EBADGER:
             DefineNumericMacro ("__EBADGER__", 1);
             break;
-            
+
         case TGT_RP6502:
             DefineNumericMacro ("__RP6502__", 1);
             break;

a7e509cd43f4ff54a11873757615f8bf68d49940
mrdudz <mrdudz@users.noreply.github.com>
2025-06-25 21:39:03 +0200

    better handling of the cursor, use kernal function to read key, not the UART directly

--- libsrc/c65/kbhit.s
+++ libsrc/c65/kbhit.s
@@ -1,16 +1,6 @@
 
-
        ; FIXME: is $d610 mega65 specific?
        ; FIXME: this should rather use the kernal (with keyboard buffer etc)
-
-        .export _cgetc
-_cgetc:
-
-:       lda $d610
-        beq :-
-        ldx #0
-        stx $d610
-        rts
 
         .export _kbhit
 _kbhit:
@@ -19,6 +9,6 @@ _kbhit:
 
         lda #1
 :
-        ldx #0
+        ldx #>$0000
         rts
 

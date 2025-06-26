cc65/cc65

commit 990d65e4e463d1dc95913bc7624369636861f7f5

Apple2: Setup IRQ/RST vectors in LC if needed
Programs running under DOS3.3 need to setup correct
reset and IRQ vectors in the language card.

libsrc/apple2/crt0.s
@@ -58,6 +58,7 @@ init:   ldx     #zpspace-1
         ; Check for ProDOS.
         ldy     $BF00           ; MLI call entry point
         cpy     #$4C            ; Is MLI present? (JMP opcode)
+        php                     ; Remember whether we're running ProDOS
         bne     basic
 
         ; Check the ProDOS system bit map.
@@ -99,7 +100,20 @@ basic:  lda     HIMEM
         bit     $C081
         bit     $C081
 
-        ; Set the source start address.
+        plp                     ; Are we running ProDOS?
+        beq     :+              ; Yes, no need to patch vectors
+
+        lda     #<reset_6502
+        ldx     #>reset_6502
+        sta     ROM_RST
+        stx     ROM_RST+1
+
+        lda     #<irq_6502
+        ldx     #>irq_6502
+        sta     ROM_IRQ
+        stx     ROM_IRQ+1
+
+:       ; Set the source start address.
         ; Aka __LC_LOAD__ iff segment LC exists.
         lda     #<(__ONCE_LOAD__ + __ONCE_SIZE__)
         ldy     #>(__ONCE_LOAD__ + __ONCE_SIZE__)
@@ -143,6 +157,14 @@ return: rts
 quit:   jsr     $BF00           ; MLI call entry point
         .byte   $65             ; Quit
         .word   q_param
+
+reset_6502:                     ; Used with DOS3.3 programs
+        bit     $C082           ; Switch in ROM
+        jmp     (ROM_RST)       ; Jump to ROM's RESET vector
+
+irq_6502:                       ; Used with DOS3.3 programs
+        bit     $C082           ; Switch in ROM
+        jmp     (ROM_IRQ)       ; Jump to ROM's IRQ/BRK vector
 
 ; ------------------------------------------------------------------------
 

asminc/apple2.inc
@@ -24,6 +24,8 @@ DOSWARM :=      $03D0   ; DOS warmstart vector
 BRKVec  :=      $03F0   ; Break vector
 SOFTEV  :=      $03F2   ; Vector for warm start
 PWREDUP :=      $03F4   ; This must be = EOR #$A5 of SOFTEV+1
+ROM_RST :=      $FFFC   ; 6502 reset vector
+ROM_IRQ :=      $FFFE   ; 6502 IRQ vector
 
 ;-----------------------------------------------------------------------------
 ; 80 column firmware

doc/apple2.sgml
@@ -87,7 +87,7 @@ several useful settings:
   exit then a plain vanilla ProDOS 8 doesn't make use of the Language Card bank
   2 at all.
 
-  <tag>LC address: &dollar;D000, LC size: &dollar;3000</tag>
+  <tag>LC address: &dollar;D000, LC size: &dollar;2FFC</tag>
   For plain vanilla DOS 3.3 which doesn't make use of the Language Card at all.
 
 </descrip><p>

doc/apple2enh.sgml
@@ -88,7 +88,7 @@ several useful settings:
   exit then a plain vanilla ProDOS 8 doesn't make use of the Language Card bank
   2 at all.
 
-  <tag>LC address: &dollar;D000, LC size: &dollar;3000</tag>
+  <tag>LC address: &dollar;D000, LC size: &dollar;2FFC</tag>
   For plain vanilla DOS 3.3 which doesn't make use of the Language Card at all.
 
 </descrip><p>

commit 3ec6fda8a6699715726af1c95247fc5fabec42ba

Apple2: Fix permanently disabled IRQ
Regression introduced in 990d65e:
Pushing status, initializing IRQ handler (which enables IRQ)
then pulling status re-disables IRQ.

libsrc/apple2/crt0.s
@@ -56,9 +56,10 @@ init:   ldx     #zpspace-1
         bpl     :-
 
         ; Check for ProDOS.
-        ldy     $BF00           ; MLI call entry point
-        cpy     #$4C            ; Is MLI present? (JMP opcode)
-        php                     ; Remember whether we're running ProDOS
+        lda     $BF00           ; MLI call entry point
+        sec
+        sbc     #$4C            ; Is MLI present? (JMP opcode)
+        pha                     ; Backup the result for later
         bne     basic
 
         ; Check the ProDOS system bit map.
@@ -100,8 +101,8 @@ basic:  lda     HIMEM
         bit     $C081
         bit     $C081
 
-        plp                     ; Are we running ProDOS?
-        beq     :+              ; Yes, no need to patch vectors
+        pla                     ; If not running ProDOS, we need to patch 6502 vectors.
+        beq     :+
 
         lda     #<reset_6502
         ldx     #>reset_6502
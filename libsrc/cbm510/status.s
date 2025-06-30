26cf19642c2b7ec0ae4e11ade6b90390811fa3bd
mrdudz <mrdudz@users.noreply.github.com>
2025-06-30 18:39:33 +0200

    initial patch from #1972

---
+++ libsrc/cbm510/status.s
@@ -0,0 +1,21 @@
+;
+; Stefan Haubenthal, 2023-01-16
+;
+
+        .export         initst
+
+        .include        "extzp.inc"
+        .include        "cbm510.inc"
+
+.proc   initst
+
+        ldx     IndReg
+        ldy     #$0F
+        sty     IndReg          ; Switch to the system bank
+        ldy     #STATUS
+        lda     #$00            ; Initialize value
+        sta     (sysp0),y
+        stx     IndReg
+        rts
+
+.endproc

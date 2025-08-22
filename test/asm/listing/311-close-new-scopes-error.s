ce20b6aa4833c0de74b1c617a999622b82357fa9
Joshua Bell <inexorabletash@gmail.com>
2025-08-22 09:57:45 -0700

    Explain the expected failures in 311

--- test/asm/listing/311-close-new-scopes-error.s
+++ test/asm/listing/311-close-new-scopes-error.s
@@ -1,5 +1,12 @@
+;;; Regression test to ensure that newly inferred scopes are closed.
+
 .scope foo
 start:
+        ;; Since `bar` is not a known scope at this point, it is
+        ;; inferred to be `::foo::bar`, and since `::foo::bar::start`
+        ;; does not exist this will produce an error. Importantly,
+        ;; it does not resolve to `::foo::start` which would be
+        ;; incorrect.
         jmp bar::start
 .endscope
 

47c71604760c117b5bf8aefbdfccaf1b3f36fd82
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-10-21 20:58:50 -0700

    getcwd

--- libsrc/rp6502/f_getcwd.c
+++ libsrc/rp6502/f_getcwd.c
@@ -2,10 +2,10 @@
 #include <errno.h>
 #include <string.h>
 
-int __fastcall__ f_getcwd (char* name, unsigned len)
+int __fastcall__ f_getcwd (char* name, int size)
 {
     int i, ax;
-    ria_set_ax (len);
+    ria_set_ax (size);
     ax = ria_call_int (RIA_OP_GETCWD);
     for (i = 0; i < ax; i++) {
         name[i] = ria_pop_char ();

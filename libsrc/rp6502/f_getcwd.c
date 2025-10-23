4cb40a415561a89155e5ab9089f3681e5dbdb09d
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-10-22 18:51:01 -0700

    f_getcwd

--- libsrc/rp6502/f_getcwd.c
+++ libsrc/rp6502/f_getcwd.c
@@ -5,8 +5,12 @@
 int __fastcall__ f_getcwd (char* name, int size)
 {
     int i, ax;
-    ria_set_ax (size);
     ax = ria_call_int (RIA_OP_GETCWD);
+    if (ax > size) {
+        RIA.op = RIA_OP_ZXSTACK;
+        errno = ENOMEM;
+        return -1;
+    }
     for (i = 0; i < ax; i++) {
         name[i] = ria_pop_char ();
     }

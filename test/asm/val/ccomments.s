f3dc8a511b31808ae8fd1541bd5329962c6c47c5
Cole Campbell <cole.campbell@twistedlogik.net>
2026-07-18 08:49:11 -0400

    Assert value returned by PC division

--- test/asm/val/ccomments.s
+++ test/asm/val/ccomments.s
@@ -2,8 +2,11 @@
 
 ; both expressions are valid with c_comments disabled
 .feature c_comments -
-.word 8 / *
-.word 8/*  
+
+.org $0002
+.assert 8 / * = 4, error, "8 / * should evaluate to 4"
+.assert 8/* = 4, error, "8/* should evaluate to 4"
+.reloc 
 
 _main:
     lda #0
